f2c1f85fb8d56d0ea71fb10505e1fe4f5286b719
Olivier MARSOL <olivier.marsol.ext@culture.gouv.fr>
2016-10-31 18:21:08 +0100

    test fix for clientv2 && truststore on ihm-demo

--- deployment/generate_stores.sh
+++ deployment/generate_stores.sh
@@ -10,6 +10,25 @@ ENVIRONNEMENT=${1}
 echo "Sourcer les informations nécessaires dans vault.yml"
 eval $(ansible-vault view $(dirname $0)/environments-rpm/group_vars/all/vault.yml | sed -e 's/: /=/')
 
+for i in $(ansible -i environments-rpm/hosts.${ENVIRONNEMENT} --list-hosts hosts-ihm-demo --ask-vault-pass| sed "1 d"); do
+	echo "Génération du keystore de ihm-demo"
+	echo "	Génération pour ${i}..."
+	echo "Génération du truststore de ${j}-external..."
+	#generationstore ${REPERTOIRE_CERTIFICAT}/server/hosts/${i}/truststore_ingest-external.jks truststore_ingest-external ${TrustStorePassword_ingest_external}
+	echo "	Import des CA server dans truststore de ${j}-external..."
+	echo "		... import CA server root..."
+	addcainjks ${REPERTOIRE_CERTIFICAT}/server/hosts/${i}/truststore_ihm-demo.jks $(eval "echo \$TrustStore_ihm_demo_password") ${REPERTOIRE_CA}/server/ca.crt ca_server_root_crt
+	echo "		... import CA server intermediate..."
+	addcainjks ${REPERTOIRE_CERTIFICAT}/server/hosts/${i}/truststore_ihm-demo.jks $(eval "echo \$TrustStore_ihm_demo_password") ${REPERTOIRE_CA}/server_intermediate/ca.crt ca_server_interm_root_crt
+	echo "		... import CA client root..."
+	addcainjks ${REPERTOIRE_CERTIFICAT}/server/hosts/${i}/truststore_ihm-demo.jks $(eval "echo \$TrustStore_ihm_demo_password") ${REPERTOIRE_CA}/client/ca.crt ca_client_root_crt
+	echo "		... import CA client intermediate..."
+	addcainjks ${REPERTOIRE_CERTIFICAT}/server/hosts/${i}/truststore_ihm-demo.jks $(eval "echo \$TrustStore_ihm_demo_password") ${REPERTOIRE_CA}/client_intermediate/ca.crt ca_client_interm_root_crt
+
+	echo "Fin de génération du trustore de ${j}-external"
+	echo "------------------------------------------------"
+done
+
 for j in ingest access; do
 	for i in $(ansible -i environments-rpm/hosts.${ENVIRONNEMENT} --list-hosts hosts-${j}-external --ask-vault-pass| sed "1 d"); do
 		echo "Génération du keystore de access-external"
