94416482ec192515906f37d173f2f51665f588fe
Olivier MARSOL <olivier.marsol.ext@culture.gouv.fr>
2016-11-02 16:46:26 +0100

    Vitam logo in documentation mongo express fixes mongo collectiosn fixes

--- doc/fr/conf-common.py
+++ doc/fr/conf-common.py
@@ -119,7 +119,7 @@ html_theme_path = ["_themes"]
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = '../LogoVitamGrand2.png'
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
@@ -228,7 +228,7 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-#latex_logo = None
+latex_logo = '../LogoVitamGrand2.png'
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
