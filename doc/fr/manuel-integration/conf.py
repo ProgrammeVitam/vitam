900b2cde71f75bad44da98475c3acd0814c6f952
Kristopher Waltzer <kristopher.waltzer@thalesgroup.com>
2017-05-10 11:06:35 +0200

    Added documentation : 'manuel-integration'

---
+++ doc/fr/manuel-integration/conf.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+#
+# General information about the project.
+project = u'VITAM - Manuel Intégraption Applicative'
+doc_id = 'vitam-manuel-integration'
+
+execfile("../conf-common.py")
---
+++ doc/fr/manuel-integration/index.rst
@@ -0,0 +1,14 @@
+###########
+|doc_title|
+###########
+
+.. toctree::
+   :maxdepth: 2
+
+   Vitam.rst
+   API.rst
+   standard.rst
+   DSL.rst
+   dsl-introduction.rst
+   Exemples.rst
+   exemple-dsl.rst
