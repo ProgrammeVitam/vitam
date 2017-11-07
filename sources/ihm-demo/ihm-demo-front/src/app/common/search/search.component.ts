13e561754f8c45c77b94bf840bedbfc1e4d49838
aparis <antoine.paris@smile.fr>
2017-11-07 14:20:24 +0100

    - Fix date-picker en to fr in Search Form, ArchiveUnit and rules components

--- sources/ihm-demo/ihm-demo-front/src/app/common/search/search.component.ts
+++ sources/ihm-demo/ihm-demo-front/src/app/common/search/search.component.ts
@@ -28,6 +28,19 @@ export class SearchComponent implements OnInit {
   @Output() responseEvent: EventEmitter<any> = new EventEmitter<any>();
   @Output() panelButtonEvent: EventEmitter<any> = new EventEmitter<any>();
 
+
+
+  frLocale = {
+    dayNames: ["Dimanche","Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"],
+    dayNamesShort: ["Dim.", "Lun.", "Mar.", "Mer.", "Jeu.", "Ven.", "Sam."],
+    dayNamesMin: ["Di","Lu","Ma","Me","Je","Ve","Sa"],
+    monthNames: [ "Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Aout","Septembre","Octobre","Novembre","Décembre" ],
+    monthNamesShort: [ "Jan", "Fév", "Mars", "Avr", "Mai", "Juin","Juil", "Aou", "Sep", "Oct", "Nov", "Dec" ],
+    firstDayOfWeek: 1,
+    today: "Aujourd'hui",
+    clear: 'Vider'
+  };
+
   constructor(private accessContractService: AccessContractService) {
     this.accessContractService.getUpdate().subscribe(
       () => {
