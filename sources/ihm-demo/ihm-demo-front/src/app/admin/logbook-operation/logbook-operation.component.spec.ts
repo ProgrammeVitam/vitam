9c03df2948f5307614bde5b82a7664de33eb0058
Clément MASSARD <clement.massard.ext@culture.gouv.fr>
2017-12-08 11:46:05 +0100

    Fixed operation status label in detail page when operation is pending

--- sources/ihm-demo/ihm-demo-front/src/app/admin/logbook-operation/logbook-operation.component.spec.ts
+++ sources/ihm-demo/ihm-demo-front/src/app/admin/logbook-operation/logbook-operation.component.spec.ts
@@ -12,6 +12,71 @@ const LogbookServiceStub = {
   getResults: () => Observable.of(new VitamResponse())
 };
 
+const item = {
+    "evId": "aecaaaaaacffskldaajwuak7toeyuxyaaaaq",
+    "evParentId": null,
+    "evType": "STP_OP_SECURISATION",
+    "evDateTime": "2017-11-08T12:10:03.240",
+    "evIdProc": "aecaaaaaacffskldaajwuak7toeyuxyaaaaq",
+    "evTypeProc": "TRACEABILITY",
+    "outcome": "STARTED",
+    "outDetail": "STP_OP_SECURISATION.STARTED",
+    "outMessg": "Début du processus de sécurisation des journaux",
+    "agId": "{\"Name\":\"vitam-iaas-app-04\",\"Role\":\"logbook\",\"ServerId\":1381575011,\"SiteId\":1,\"GlobalPlatformId\":173615459}",
+    "obId": null,
+    "evDetData": "{\"LogType\":\"OPERATION\",\"StartDate\":\"207-11-08T11:53:14.105\",\"EndDate\":\"2017-11-08T12:10:03.240\",\"Hash\":\"mockHash\",\"TimeStampToken\":\"mockTimeStampToken\",\"NumberOfElement\":196,\"FileName\":\"0_LogbookOperation_20171108_121003.zip\",\"Size\":2109403,\"DigestAlgorithm\":\"SHA512\"}",
+    "rightsStatementIdentifier": null,
+    "agIdApp": null,
+    "evIdAppSession": null,
+    "evIdReq": "aecaaaaaacffskldaajwuak7toeyuxyaaaaq",
+    "agIdExt": null,
+    "obIdReq": null,
+    "obIdIn": null,
+    "#id": null,
+    "#tenant": null,
+    "events": [{
+        "evId": "aedqaaaaacffskldaajwuak7toeyzfqaaaaq",
+        "evParentId": null,
+        "evType": "OP_SECURISATION_TIMESTAMP",
+        "evDateTime": "2017-11-08T12:10:03.798",
+        "evIdProc": "aecaaaaaacffskldaajwuak7toeyuxyaaaaq",
+        "evTypeProc": "TRACEABILITY",
+        "outcome": "OK",
+        "outDetail": "OP_SECURISATION_TIMESTAMP.OK",
+        "outMessg": "Succès de la création du tampon d'horodatage de l'ensemble des journaux",
+        "agId": "{\"Name\":\"vitam-iaas-app-04\",\"Role\":\"logbook\",\"ServerId\":1381575011,\"SiteId\":1,\"GlobalPlatformId\":173615459}",
+        "obId": null,
+        "evDetData": null,
+        "rightsStatementIdentifier": null,
+        "agIdApp": null,
+        "evIdAppSession": null,
+        "evIdReq": "aecaaaaaacffskldaajwuak7toeyuxyaaaaq",
+        "agIdExt": null,
+        "obIdReq": null,
+        "obIdIn": null
+    }, {
+        "evId": "aedqaaaaacffskldaajwuak7toey6jiaaaaq",
+        "evParentId": null,
+        "evType": "OP_SECURISATION_STORAGE",
+        "evDateTime": "2017-11-08T12:10:04.453",
+        "evIdProc": "aecaaaaaacffskldaajwuak7toeyuxyaaaaq",
+        "evTypeProc": "TRACEABILITY",
+        "outcome": "OK",
+        "outDetail": "OP_SECURISATION_STORAGE.OK",
+        "outMessg": "Succès de l'enregistrement des journaux sur les offres de stockage",
+        "agId": "{\"Name\":\"vitam-iaas-app-04\",\"Role\":\"logbook\",\"ServerId\":1381575011,\"SiteId\":1,\"GlobalPlatformId\":173615459}",
+        "obId": null,
+        "evDetData": null,
+        "rightsStatementIdentifier": null,
+        "agIdApp": null,
+        "evIdAppSession": null,
+        "evIdReq": "aecaaaaaacffskldaajwuak7toeyuxyaaaaq",
+        "agIdExt": null,
+        "obIdReq": null,
+        "obIdIn": null
+    }]
+};
+
 describe('LogbookOperationComponent', () => {
   let component: LogbookOperationComponent;
   let fixture: ComponentFixture<LogbookOperationComponent>;
@@ -37,5 +102,31 @@ describe('LogbookOperationComponent', () => {
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should return operation status', () => {
+    expect(LogbookOperationComponent.getOperationStatus(item)).toEqual('En cours');
+    item.events.push({
+          "evId": "aedqaaaaacffskldaajwuak7toey6pyaaaaq",
+          "evParentId": null,
+          "evType": "STP_OP_SECURISATION",
+          "evDateTime": "2017-11-08T12:10:04.479",
+          "evIdProc": "aecaaaaaacffskldaajwuak7toeyuxyaaaaq",
+          "evTypeProc": "TRACEABILITY",
+          "outcome": "OK",
+          "outDetail": "STP_OP_SECURISATION.OK",
+          "outMessg": "Succès du processus de sécurisation des journaux",
+          "agId": "{\"Name\":\"vitam-iaas-app-04\",\"Role\":\"logbook\",\"ServerId\":1381575011,\"SiteId\":1,\"GlobalPlatformId\":173615459}",
+          "obId": null,
+          "evDetData": "{\"LogType\":\"OPERATION\",\"StartDate\":\"2017-11-08T11:53:14.105\",\"EndDate\":\"2017-11-08T12:10:03.240\",\"Hash\":\"mockHash\",\"TimeStampToken\":\"mockTimeStampToken\",\"NumberOfElement\":196,\"FileName\":\"0_LogbookOperation_20171108_121003.zip\",\"Size\":2109403,\"DigestAlgorithm\":\"SHA512\"}",
+          "rightsStatementIdentifier": null,
+          "agIdApp": null,
+          "evIdAppSession": null,
+          "evIdReq": "aecaaaaaacffskldaajwuak7toeyuxyaaaaq",
+          "agIdExt": null,
+          "obIdReq": null,
+          "obIdIn": null
+    });
+    expect(LogbookOperationComponent.getOperationStatus(item)).toEqual('OK');
   });
 });
