d892b824402c704a6b63c65ec09ddfceaacc9f89
aparis <antoine.paris@smile.fr>
2017-12-13 16:45:36 +0100

    Fix bad redirect url in format details tab

--- sources/ihm-demo/ihm-demo-front/src/app/referentials/details/format/format.component.ts
+++ sources/ihm-demo/ihm-demo-front/src/app/referentials/details/format/format.component.ts
@@ -1,19 +1,18 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { plainToClass } from 'class-transformer';
-import { Title } from '@angular/platform-browser';
+import {Component} from "@angular/core";
+import {ActivatedRoute} from "@angular/router";
+import {plainToClass} from "class-transformer";
+import {Title} from "@angular/platform-browser";
 
-import { BreadcrumbService, BreadcrumbElement } from "../../../common/breadcrumb.service";
-import { ReferentialsService } from "../../referentials.service";
-import { Format } from "./format";
-import { DateService } from '../../../common/utils/date.service';
-import { PageComponent } from "../../../common/page/page-component";
+import {BreadcrumbService} from "../../../common/breadcrumb.service";
+import {ReferentialsService} from "../../referentials.service";
+import {Format} from "./format";
+import {PageComponent} from "../../../common/page/page-component";
 
 const FORMAT_KEY_TRANSLATION = {
   Name: 'Intitulé',
-  HasPriorityOverFileFormatID : 'Priorité sur les versions précédentes',
-  MIMEType : 'MIME types',
-  VersionPronom : 'Version de Pronom',
+  HasPriorityOverFileFormatID: 'Priorité sur les versions précédentes',
+  MIMEType: 'MIME types',
+  VersionPronom: 'Version de Pronom',
   CreatedDate: "Date de création"
 };
 
@@ -23,27 +22,28 @@ const FORMAT_KEY_TRANSLATION = {
   styleUrls: ['./format.component.css']
 })
 
-export class FormatComponent  extends PageComponent {
+export class FormatComponent extends PageComponent {
 
   id: string;
-  format : Format;
-  pronomLink : string;
-  arrayOfKeys : string[];
-  constructor(private activatedRoute: ActivatedRoute, private router : Router,
-              public titleService: Title, public breadcrumbService: BreadcrumbService,
-              private searchReferentialsService : ReferentialsService) {
+  format: Format;
+  pronomLink: string;
+  arrayOfKeys: string[];
+
+  constructor(private activatedRoute: ActivatedRoute, public titleService: Title,
+              public breadcrumbService: BreadcrumbService,
+              private searchReferentialsService: ReferentialsService) {
     super('Détail du format ', [], titleService, breadcrumbService);
   }
 
   pageOnInit() {
-    this.activatedRoute.params.subscribe( params => {
+    this.activatedRoute.params.subscribe(params => {
       this.id = params['id'];
       this.searchReferentialsService.getFormatById(this.id).subscribe((value) => {
         this.format = plainToClass(Format, value.$results)[0];
         let keys = Object.keys(this.format);
         let format = this.format;
         this.pronomLink = 'http://www.nationalarchives.gov.uk/PRONOM/' + this.format.PUID;
-        this.arrayOfKeys = keys.filter(function(key) {
+        this.arrayOfKeys = keys.filter(function (key) {
           return key != '_id' && !!format[key] && format[key].length > 0;
         });
       });
@@ -69,7 +69,7 @@ export class FormatComponent  extends PageComponent {
     window.open(this.pronomLink);
   }
 
-  encode(puid : string) {
-    return  encodeURIComponent(puid);
+  encode(puid: string) {
+    return encodeURIComponent(puid);
   }
 }
