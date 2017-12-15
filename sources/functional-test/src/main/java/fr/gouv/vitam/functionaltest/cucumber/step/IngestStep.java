cf85b176e217535c3b26bf6d3ef67b384aeec2c5
Gaëlle Fournier <gaelle.fournier.workf@gmail.com>
2017-12-15 17:14:40 +0100

    Item #3921 : modify TNR step for logbook lifecycle + refactor logbook operation
    * Refactor logbook operation + lifecycle to download logbook before checking them
    * Use same steps phrase for logbook operation and lifecycle

--- sources/functional-test/src/main/java/fr/gouv/vitam/functionaltest/cucumber/step/IngestStep.java
+++ sources/functional-test/src/main/java/fr/gouv/vitam/functionaltest/cucumber/step/IngestStep.java
@@ -40,36 +40,22 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.StringUtils;
-import org.assertj.core.api.AutoCloseableSoftAssertions;
-import org.assertj.core.api.Fail;
-
-import com.google.common.collect.Iterables;
 
 import cucumber.api.java.After;
 import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import fr.gouv.vitam.access.external.client.VitamPoolingClient;
 import fr.gouv.vitam.common.GlobalDataRest;
 import fr.gouv.vitam.common.client.VitamContext;
-import fr.gouv.vitam.common.database.builder.request.single.Select;
-import fr.gouv.vitam.common.error.VitamError;
-import fr.gouv.vitam.common.exception.InvalidParseOperationException;
-import fr.gouv.vitam.common.exception.VitamClientException;
 import fr.gouv.vitam.common.exception.VitamException;
 import fr.gouv.vitam.common.logging.VitamLogger;
 import fr.gouv.vitam.common.logging.VitamLoggerFactory;
 import fr.gouv.vitam.common.model.ProcessAction;
 import fr.gouv.vitam.common.model.ProcessState;
 import fr.gouv.vitam.common.model.RequestResponse;
-import fr.gouv.vitam.common.model.RequestResponseOK;
-import fr.gouv.vitam.common.model.logbook.LogbookEventOperation;
-import fr.gouv.vitam.common.model.logbook.LogbookOperation;
 import fr.gouv.vitam.ingest.external.api.exception.IngestExternalException;
 import fr.gouv.vitam.tools.SipTool;
 
@@ -120,7 +106,6 @@ public class IngestStep {
             }
             assertThat(operationId).as(format("%s not found for request", X_REQUEST_ID)).isNotNull();
         }
-
     }
 
     /**
@@ -184,129 +169,6 @@ public class IngestStep {
         }
     }
 
-    /**
-     * check if the status is valid for a list of event type according to logbook operation
-     *
-     * @param eventNames list of event
-     * @param eventStatus status of event
-     * @throws VitamClientException
-     * @throws InvalidParseOperationException
-     */
-    @Then("^le[s]? statut[s]? (?:de l'événement|des événements) (.*) (?:est|sont) (.*)$")
-    public void the_status_are(List<String> eventNames, String eventStatus)
-        throws VitamClientException, InvalidParseOperationException {
-        RequestResponse<LogbookOperation> requestResponse =
-            world.getAccessClient()
-                .selectOperationbyId(new VitamContext(world.getTenantId()).setAccessContract(world.getContractId())
-                    .setApplicationSessionId(world.getApplicationSessionId()),
-                    world.getOperationId(), new Select().getFinalSelectById());
-
-        if (requestResponse.isOk()) {
-            RequestResponseOK<LogbookOperation> requestResponseOK =
-                (RequestResponseOK<LogbookOperation>) requestResponse;
-
-            List<LogbookEventOperation> actual = requestResponseOK.getFirstResult().getEvents();
-            try (AutoCloseableSoftAssertions softly = new AutoCloseableSoftAssertions()) {
-                for (String eventName : eventNames) {
-                    List<LogbookEventOperation> events =
-                        actual.stream().filter(event -> eventName.equals(event.getEvType()))
-                            .filter(event -> !"STARTED".equals(event.getOutcome()))
-                            .collect(Collectors.toList());
-
-                    softly.assertThat(events).as("event %s is not present or finish.", eventName).hasSize(1);
-                    LogbookEventOperation onlyElement = Iterables.getOnlyElement(events);
-
-                    String currentStatus = onlyElement.getOutcome();
-                    softly.assertThat(currentStatus)
-                        .as("event %s has status %s but excepted status is %s.", eventName, currentStatus, eventStatus)
-                        .isEqualTo(eventStatus);
-                }
-            }
-        } else {
-            VitamError error = (VitamError) requestResponse;
-            LOGGER.error(String.format("logbook operation return a vitam error for operationId: %s, requestId is %s",
-                world.getOperationId(), error.getCode()));
-            Fail.fail("cannot find logbook with id: " + world.getOperationId());
-        }
-    }
-
-    @Then("^l'outcome détail de l'événement (.*) est (.*)$")
-    public void the_outcome_detail_is(String eventName, String eventOutDetail) throws Throwable {
-        RequestResponse<LogbookOperation> requestResponse =
-            world.getAccessClient()
-                .selectOperationbyId(new VitamContext(world.getTenantId()).setAccessContract(world.getContractId())
-                    .setApplicationSessionId(world.getApplicationSessionId()),
-                    world.getOperationId(), new Select().getFinalSelectById());
-
-        if (requestResponse.isOk()) {
-            RequestResponseOK<LogbookOperation> requestResponseOK =
-                (RequestResponseOK<LogbookOperation>) requestResponse;
-
-            List<LogbookEventOperation> actual = requestResponseOK.getFirstResult().getEvents();
-
-            try (AutoCloseableSoftAssertions softly = new AutoCloseableSoftAssertions()) {
-                List<LogbookEventOperation> events =
-                    actual.stream().filter(event -> eventName.equals(event.getEvType()))
-                        .filter(event -> !"STARTED".equals(event.getOutcome()))
-                        .collect(Collectors.toList());
-
-                softly.assertThat(events).as("event %s is not present or finish.", eventName).hasSize(1);
-                LogbookEventOperation onlyElement = Iterables.getOnlyElement(events);
-
-                String currentOutDetail = onlyElement.getOutDetail();
-                softly.assertThat(currentOutDetail)
-                    .as("event %s has status %s but excepted status is %s.", eventName, currentOutDetail,
-                        eventOutDetail)
-                    .isEqualTo(eventOutDetail);
-            }
-        }
-    }
-
-
-    /**
-     * check if the outcome detail is valid for an event type according to logbook operation
-     *
-     * @param eventName the event
-     * @param eventResults otucome detail of the event
-     * @throws VitamClientException
-     * @throws InvalidParseOperationException
-     */
-    @Then("^le résultat de l'événement (.*) est (.*)$")
-    public void the_results_are(String eventName, String eventResults)
-        throws VitamClientException, InvalidParseOperationException {
-        RequestResponse<LogbookOperation> requestResponse =
-            world.getAccessClient()
-                .selectOperationbyId(new VitamContext(world.getTenantId()).setAccessContract(world.getContractId())
-                    .setApplicationSessionId(world.getApplicationSessionId()),
-                    world.getOperationId(), new Select().getFinalSelectById());
-
-        if (requestResponse.isOk()) {
-            RequestResponseOK<LogbookOperation> requestResponseOK =
-                (RequestResponseOK<LogbookOperation>) requestResponse;
-
-            List<LogbookEventOperation> actual = requestResponseOK.getFirstResult().getEvents();
-            try (AutoCloseableSoftAssertions softly = new AutoCloseableSoftAssertions()) {
-                List<LogbookEventOperation> events =
-                    actual.stream().filter(event -> eventName.equals(event.getEvType()))
-                        .filter(event -> !"STARTED".equals(event.getOutcome()))
-                        .collect(Collectors.toList());
-
-                softly.assertThat(events).as("event %s is not present or finish.", eventName).hasSize(1);
-                LogbookEventOperation onlyElement = Iterables.getOnlyElement(events);
-
-                String currentResult = onlyElement.getOutDetail();
-                softly.assertThat(currentResult)
-                    .as("event %s has outcome detail %s but excepted outcome detail is %s.", eventName, currentResult,
-                        eventResults)
-                    .isEqualTo(eventResults);
-            }
-        } else {
-            VitamError error = (VitamError) requestResponse;
-            LOGGER.error(String.format("logbook operation return a vitam error for operationId: %s, requestId is %s",
-                world.getOperationId(), error.getCode()));
-            Fail.fail("cannot find logbook with id: " + world.getOperationId());
-        }
-    }
 
     @When("je construit le sip de rattachement avec le template")
     public void build_the_attachenment() throws IOException {
