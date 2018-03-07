c839a8bb8b33b143807d77541d97dc772309cbd4
hbamri <hassen.bamri@gmail.com>
2018-03-07 12:17:39 +0100

    Bug 4285 : Fix report link

--- sources/ihm-demo/ihm-demo-front/src/app/admin/logbook-operation/logbook-operation.component.ts
+++ sources/ihm-demo/ihm-demo-front/src/app/admin/logbook-operation/logbook-operation.component.ts
@@ -108,7 +108,7 @@ export class LogbookOperationComponent extends PageComponent {
 
   static handleReports(item): string[] {
     const evType = item.evTypeProc.toUpperCase();
-    if (['AUDIT','EXPORT_DIP','INGEST'].indexOf(evType) > -1 || item.evType.toUpperCase() === 'STP_IMPORT_RULES' || item.evType.toUpperCase() == 'STP_IMPORT_AGENCIES') {
+    if (['AUDIT','EXPORT_DIP','INGEST'].indexOf(evType) > -1 || item.evType.toUpperCase() === 'STP_IMPORT_RULES' || item.evType.toUpperCase() == 'IMPORT_AGENCIES') {
       return ['fa-download']
     }else {
       return [];
@@ -187,7 +187,7 @@ export class LogbookOperationComponent extends PageComponent {
         logbookService.downloadDIP(item.evIdProc);
         break;
       case 'MASTERDATA':
-        if(item.evType.toUpperCase() == 'STP_IMPORT_RULES' || item.evType.toUpperCase() == 'STP_IMPORT_AGENCIES') {
+        if(item.evType.toUpperCase() == 'STP_IMPORT_RULES' || item.evType.toUpperCase() == 'IMPORT_AGENCIES') {
           logbookService.downloadReport(item.evIdProc);
         }
         break;
