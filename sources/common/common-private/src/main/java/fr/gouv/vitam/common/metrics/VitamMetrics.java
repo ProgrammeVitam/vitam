fb69aca72ef527a5a2c43db9741262268df71c85
Olivier MARSOL <olivier.marsol.ext@culture.gouv.fr>
2018-08-07 12:08:39 +0200

    Test adding a special reporter ; fork

--- sources/common/common-private/src/main/java/fr/gouv/vitam/common/metrics/VitamMetrics.java
+++ sources/common/common-private/src/main/java/fr/gouv/vitam/common/metrics/VitamMetrics.java
@@ -33,7 +33,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
-import org.elasticsearch.metrics.ElasticsearchReporter;
+import fr.gouv.vitam.common.private.VitamElasticsearchReporter;
 
 import com.codahale.metrics.ScheduledReporter;
 import com.codahale.metrics.jvm.BufferPoolMetricSet;
