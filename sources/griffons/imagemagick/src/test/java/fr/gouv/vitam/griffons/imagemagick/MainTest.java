36e9fd80fe4785cd2d4a45ea5a371ec9c8035eb2
Jbbouille <beanbaptiste.petit@gmail.com>
2018-10-17 15:41:13 +0200

    Process initialization.

---
+++ sources/griffons/imagemagick/src/test/java/fr/gouv/vitam/griffons/imagemagick/MainTest.java
@@ -0,0 +1,279 @@
+package fr.gouv.vitam.griffons.imagemagick;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import fr.gouv.vitam.griffons.imagemagick.pojo.Action;
+import fr.gouv.vitam.griffons.imagemagick.pojo.BatchStatus;
+import fr.gouv.vitam.griffons.imagemagick.pojo.Input;
+import fr.gouv.vitam.griffons.imagemagick.pojo.Output;
+import fr.gouv.vitam.griffons.imagemagick.pojo.Outputs;
+import fr.gouv.vitam.griffons.imagemagick.pojo.Parameters;
+import fr.gouv.vitam.griffons.imagemagick.pojo.Values;
+import fr.gouv.vitam.griffons.imagemagick.status.GriffonStatus;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.FileAttribute;
+import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.attribute.PosixFilePermissions;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static fr.gouv.vitam.griffons.imagemagick.BatchProcessor.inputFilesDirName;
+import static fr.gouv.vitam.griffons.imagemagick.BatchProcessor.parametersFileName;
+import static fr.gouv.vitam.griffons.imagemagick.BatchProcessor.resultFileName;
+import static fr.gouv.vitam.griffons.imagemagick.Main.ID;
+import static fr.gouv.vitam.griffons.imagemagick.status.ActionType.ANALYSE;
+import static fr.gouv.vitam.griffons.imagemagick.status.ActionType.EXTRACT;
+import static fr.gouv.vitam.griffons.imagemagick.status.ActionType.GENERATE;
+import static fr.gouv.vitam.griffons.imagemagick.status.ActionType.IDENTIFY;
+import static fr.gouv.vitam.griffons.imagemagick.status.AnalyseResult.NOT_VALID;
+import static fr.gouv.vitam.griffons.imagemagick.status.AnalyseResult.VALID_ALL;
+import static fr.gouv.vitam.griffons.imagemagick.status.GriffonStatus.ERROR;
+import static fr.gouv.vitam.griffons.imagemagick.status.GriffonStatus.OK;
+import static fr.gouv.vitam.griffons.imagemagick.status.GriffonStatus.WARNING;
+import static java.nio.file.LinkOption.NOFOLLOW_LINKS;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+public class MainTest {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Rule
+    public TemporaryFolder tmpGriffonFolder = new TemporaryFolder();
+    private String batchName = "batch-test";
+
+    @Test
+    public void should_GENERATE_gif_picture() throws Exception {
+        // Given
+        Input input = new Input("280px-PNG_transparency_demonstration_1.png", "fmt/11");
+        Action action = new Action(GENERATE, new Values("GIF", Collections.emptyList()));
+        generateBatch(action, input);
+
+        String execPath = tmpGriffonFolder.getRoot().getPath();
+        BatchProcessor batchProcessor = new BatchProcessor(execPath, objectMapper);
+
+        // When
+        Main.exec(batchProcessor, execPath);
+
+        // Then
+        Outputs outputs = getOutputs();
+        List<Output> actual = outputs.getOutputs().get(input.getName());
+
+        assertThat(actual).hasSize(1);
+        assertThat(actual).extracting(Output::getAction).containsExactly(GENERATE);
+        assertThat(actual).extracting(Output::getOutputName).containsExactly(String.format("%s-%s.%s", GENERATE.name(), input.getName(), action.getValues().getExtension()));
+        assertThat(actual).extracting(Output::getStatus).containsExactly(OK);
+    }
+
+    @Test
+    public void should_ANALYZE_png_picture() throws Exception {
+        // Given
+        Input input = new Input("280px-PNG_transparency_demonstration_1.png", "fmt/11");
+        Action action = new Action(ANALYSE);
+        generateBatch(action, input);
+
+        String execPath = tmpGriffonFolder.getRoot().getPath();
+        BatchProcessor batchProcessor = new BatchProcessor(execPath, objectMapper);
+
+        // When
+        Main.exec(batchProcessor, execPath);
+
+        // Then
+        Outputs outputs = getOutputs();
+        List<Output> actual = outputs.getOutputs().get(input.getName());
+
+        assertThat(actual).hasSize(1);
+        assertThat(actual).extracting(Output::getAction).containsExactly(ANALYSE);
+        assertThat(actual).extracting(Output::getOutputName).containsNull();
+        assertThat(actual).extracting(Output::getStatus).containsExactly(OK);
+        assertThat(actual).extracting(Output::getAnalyseResult).containsExactly(VALID_ALL);
+    }
+
+    @Test
+    public void should_ANALYZE_jpg_picture_fall_in_warning() throws Exception {
+        // Given
+        Input input = new Input("picture-corrupted.jpg", "fmt/41");
+        Action action = new Action(ANALYSE);
+        generateBatch(action, input);
+
+        String execPath = tmpGriffonFolder.getRoot().getPath();
+        BatchProcessor batchProcessor = new BatchProcessor(execPath, objectMapper);
+
+        // When
+        Main.exec(batchProcessor, execPath);
+
+        // Then
+        Outputs outputs = getOutputs();
+        List<Output> actual = outputs.getOutputs().get(input.getName());
+
+        assertThat(actual).hasSize(1);
+        assertThat(actual).extracting(Output::getAction).containsExactly(ANALYSE);
+        assertThat(actual).extracting(Output::getOutputName).containsNull();
+        assertThat(actual).extracting(Output::getStatus).containsExactly(OK);
+        assertThat(actual).extracting(Output::getAnalyseResult).containsExactly(NOT_VALID);
+    }
+
+    @Test
+    public void should_IDENTIFY_error() throws Exception {
+        // Given
+        Input input = new Input("280px-PNG_transparency_demonstration_1.png", "fmt/11");
+        Action action = new Action(IDENTIFY);
+        generateBatch(action, input);
+
+        String execPath = tmpGriffonFolder.getRoot().getPath();
+        BatchProcessor batchProcessor = new BatchProcessor(execPath, objectMapper);
+
+        // When
+        GriffonStatus status = Main.exec(batchProcessor, execPath);
+
+        // Then
+        assertThat(status).isEqualTo(ERROR);
+        assertThat(Paths.get(tmpGriffonFolder.getRoot().getPath(), ID, batchName, resultFileName)).doesNotExist();
+    }
+
+    @Test
+    public void should_EXTRACT_metadata_picture() throws Exception {
+        // Given
+        Map<String, String> dataToExtract = new HashMap<>();
+        dataToExtract.put("AU_METADATA_DATE", "/image/properties/xmp:ModifyDate");
+        Input input = new Input("picture.jpg", "fmt/41");
+        Action action = new Action(EXTRACT, new Values(dataToExtract));
+        generateBatch(action, input);
+
+        String execPath = tmpGriffonFolder.getRoot().getPath();
+        BatchProcessor batchProcessor = new BatchProcessor(execPath, objectMapper);
+
+        // When
+        Main.exec(batchProcessor, execPath);
+
+        // Then
+        Outputs outputs = getOutputs();
+        List<Output> actual = outputs.getOutputs().get(input.getName());
+
+        assertThat(actual).hasSize(1);
+        assertThat(actual).extracting(Output::getAction).containsExactly(EXTRACT);
+        assertThat(actual).extracting(Output::getOutputName).containsExactly(String.format("%s-%s.%s", EXTRACT.name(), input.getName(), "json"));
+        assertThat(actual).extracting(Output::getStatus).containsExactly(OK);
+    }
+
+    @Test
+    public void should_return_warning_if_empty_batch_status() throws Exception {
+        // Given
+        tmpGriffonFolder.newFolder(ID);
+
+        String execPath = tmpGriffonFolder.getRoot().getPath();
+        BatchProcessor batchProcessor = new BatchProcessor(execPath, objectMapper);
+
+        // When
+        GriffonStatus status = Main.exec(batchProcessor, execPath);
+
+        // Then
+        assertThat(status).isEqualTo(WARNING);
+    }
+
+    @Test
+    public void should_return_error_if_no_parameters_file_in_batch_status() throws Exception {
+        // Given
+        tmpGriffonFolder.newFolder(ID);
+        Path batchFolder = tmpGriffonFolder.newFolder(ID, batchName).toPath();
+        Files.createFile(Paths.get(batchFolder.toString() + ".ready"));
+
+        String execPath = tmpGriffonFolder.getRoot().getPath();
+        BatchProcessor batchProcessor = new BatchProcessor(execPath, objectMapper);
+
+        // When
+        GriffonStatus status = Main.exec(batchProcessor, execPath);
+
+        // Then
+        assertThat(status).isEqualTo(ERROR);
+    }
+
+    @Test
+    public void should_execute_batch_processor_for_each_batch() throws Exception {
+        // Given
+        tmpGriffonFolder.newFolder(ID);
+
+        Path batchFolder = tmpGriffonFolder.newFolder(ID, batchName).toPath();
+        Files.createFile(Paths.get(batchFolder.toString() + ".ready"));
+
+        Path batchFolderTwo = tmpGriffonFolder.newFolder(ID, batchName + "-two").toPath();
+        Files.createFile(Paths.get(batchFolderTwo.toString() + ".ready"));
+
+        String execPath = tmpGriffonFolder.getRoot().getPath();
+        BatchProcessor batchProcessor = mock(BatchProcessor.class);
+        given(batchProcessor.execute(anyString())).willReturn(BatchStatus.ok("test", 1));
+
+        // When
+        GriffonStatus status = Main.exec(batchProcessor, execPath);
+
+        // Then
+        assertThat(status).isEqualTo(OK);
+        verify(batchProcessor, times(2)).execute(anyString());
+    }
+
+    @Test
+    public void should_only_take_ready_files_as_batch() throws Exception {
+        // Given
+        tmpGriffonFolder.newFolder(ID);
+
+        Path batchFolder = tmpGriffonFolder.newFolder(ID, batchName).toPath();
+        Files.createFile(Paths.get(batchFolder.toString() + ".ready"));
+
+        Path batchFolderTwo = tmpGriffonFolder.newFolder(ID, batchName + "-two").toPath();
+        Files.createFile(Paths.get(batchFolderTwo.toString() + ".toto"));
+
+        String execPath = tmpGriffonFolder.getRoot().getPath();
+        BatchProcessor batchProcessor = mock(BatchProcessor.class);
+        given(batchProcessor.execute(anyString())).willReturn(BatchStatus.ok("test", 1));
+
+        // When
+        GriffonStatus status = Main.exec(batchProcessor, execPath);
+
+        // Then
+        assertThat(status).isEqualTo(OK);
+        verify(batchProcessor, times(1)).execute(anyString());
+    }
+
+    private Outputs getOutputs() throws IOException {
+        return objectMapper.readValue(Paths.get(tmpGriffonFolder.getRoot().getPath(), ID, batchName, resultFileName).toFile(), Outputs.class);
+    }
+
+    private Parameters generateBatch(Action action, Input input) throws Exception {
+        tmpGriffonFolder.newFolder(ID);
+
+        Path batchFolder = tmpGriffonFolder.newFolder(ID, batchName).toPath();
+        String inputFilesFolder = tmpGriffonFolder.newFolder(ID, batchName, inputFilesDirName).toString();
+
+        Path src = new File(Object.class.getResource(String.format("/%s/batch-reference/%s/%s", ID, inputFilesDirName, input.getName())).toURI()).toPath();
+        Path target = Paths.get(inputFilesFolder, input.getName());
+        Files.copy(src, target, REPLACE_EXISTING);
+
+        Parameters parameters = new Parameters();
+        parameters.setDebug(false);
+        parameters.setRequestId("requestId");
+        parameters.setId(batchName);
+        parameters.setActions(Collections.singletonList(action));
+        parameters.setInputs(Collections.singletonList(input));
+
+        FileAttribute<Set<PosixFilePermission>> setFileAttribute = PosixFilePermissions.asFileAttribute(Files.getPosixFilePermissions(batchFolder, NOFOLLOW_LINKS));
+        File parametersFile = Files.createFile(Paths.get(batchFolder.toString(), parametersFileName), setFileAttribute).toFile();
+
+        objectMapper.writer().writeValue(parametersFile, parameters);
+        Files.createFile(Paths.get(batchFolder.toString() + ".ready"));
+        return parameters;
+    }
+}
