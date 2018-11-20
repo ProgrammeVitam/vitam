5338fc0776badc43069c4ad8b8a009e69a2a705a
Djamel HAMAS <djamel.hamas.ext@culture.gouv.fr>
2018-11-20 17:24:13 +0100

    Upgrade jackson version

--- sources/common/common-public/src/main/java/fr/gouv/vitam/common/model/RequestResponseOK.java
+++ sources/common/common-public/src/main/java/fr/gouv/vitam/common/model/RequestResponseOK.java
@@ -26,30 +26,26 @@
  *******************************************************************************/
 package fr.gouv.vitam.common.model;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-import javax.ws.rs.core.Response;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
-
 import fr.gouv.vitam.common.ParametersChecker;
 import fr.gouv.vitam.common.exception.InvalidParseOperationException;
 import fr.gouv.vitam.common.json.JsonHandler;
 import fr.gouv.vitam.common.logging.SysErrLogger;
 
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 
 /**
  * Access RequestResponseOK class contains list of results<br>
  * default results : is an empty list (immutable)
- * 
+ *
  * @param <T> Type of results
  */
-@JsonInclude(JsonInclude.Include.ALWAYS)
 public final class RequestResponseOK<T> extends RequestResponse<T> {
 
     /**
@@ -90,7 +86,7 @@ public final class RequestResponseOK<T> extends RequestResponse<T> {
 
     /**
      * Initialize from a query
-     * 
+     *
      * @param query
      */
     public RequestResponseOK(JsonNode query) {
@@ -187,9 +183,9 @@ public final class RequestResponseOK<T> extends RequestResponse<T> {
     }
 
     /**
-     * @param total of units inserted/modified as integer
+     * @param total  of units inserted/modified as integer
      * @param offset of unit in database as integer
-     * @param limit of unit per response as integer
+     * @param limit  of unit per response as integer
      * @return the RequestReponseOK with the hits are setted
      */
     public RequestResponseOK<T> setHits(long total, int offset, int limit) {
@@ -199,10 +195,10 @@ public final class RequestResponseOK<T> extends RequestResponse<T> {
 
 
     /**
-     * @param total of units inserted/modified as integer
-     * @param offset of unit in database as integer
-     * @param limit of unit per response as integer
-     * @param size of unit per response
+     * @param total    of units inserted/modified as integer
+     * @param offset   of unit in database as integer
+     * @param limit    of unit per response as integer
+     * @param size     of unit per response
      * @param scrollId of response
      * @return the RequestReponseOK with the hits are setted
      */
@@ -212,10 +208,10 @@ public final class RequestResponseOK<T> extends RequestResponse<T> {
     }
 
     /**
-     * @param total of units inserted/modified as integer
+     * @param total  of units inserted/modified as integer
      * @param offset of unit in database as integer
-     * @param limit of unit per response as integer
-     * @param size of unit per response
+     * @param limit  of unit per response as integer
+     * @param size   of unit per response
      * @return the RequestReponseOK with the hits are setted
      */
     public RequestResponseOK<T> setHits(long total, int offset, int limit, int size) {
@@ -226,7 +222,7 @@ public final class RequestResponseOK<T> extends RequestResponse<T> {
 
     /**
      * Should be used only with hints of elasticsearch
-     * 
+     *
      * @param total
      * @return
      */
@@ -295,7 +291,6 @@ public final class RequestResponseOK<T> extends RequestResponse<T> {
     }
 
     /**
-     *
      * @return True if the result is empty
      */
     @JsonIgnore
@@ -318,7 +313,7 @@ public final class RequestResponseOK<T> extends RequestResponse<T> {
      * @throws InvalidParseOperationException if parse json object exception occurred
      */
     public static <T> RequestResponseOK<T> getFromJsonNode(JsonNode node, Class<T> clazz)
-        throws InvalidParseOperationException {
+            throws InvalidParseOperationException {
         return JsonHandler.getFromString(node.toString(), RequestResponseOK.class, clazz);
     }
 
@@ -340,7 +335,7 @@ public final class RequestResponseOK<T> extends RequestResponse<T> {
 
     /**
      * transform a RequestResponse to a standard response
-     * 
+     *
      * @return Response
      */
     @Override
