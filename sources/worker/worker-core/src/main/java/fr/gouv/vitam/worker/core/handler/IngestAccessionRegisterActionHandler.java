07c007fc4d0be2f905f6cdbecf096e9d463684aa
Djamel HAMAS <djamel.hamas.ext@culture.gouv.fr>
2018-12-17 15:46:09 +0100

    Item 5392 compute accession register for preservation

---
+++ sources/worker/worker-core/src/main/java/fr/gouv/vitam/worker/core/handler/IngestAccessionRegisterActionHandler.java
@@ -0,0 +1,118 @@
+/*******************************************************************************
+ * Copyright French Prime minister Office/SGMAP/DINSIC/Vitam Program (2015-2019)
+ *
+ * contact.vitam@culture.gouv.fr
+ *
+ * This software is a computer program whose purpose is to implement a digital archiving back-office system managing
+ * high volumetry securely and efficiently.
+ *
+ * This software is governed by the CeCILL 2.1 license under French law and abiding by the rules of distribution of free
+ * software. You can use, modify and/ or redistribute the software under the terms of the CeCILL 2.1 license as
+ * circulated by CEA, CNRS and INRIA at the following URL "http://www.cecill.info".
+ *
+ * As a counterpart to the access to the source code and rights to copy, modify and redistribute granted by the license,
+ * users are provided only with a limited warranty and the software's author, the holder of the economic rights, and the
+ * successive licensors have only limited liability.
+ *
+ * In this respect, the user's attention is drawn to the risks associated with loading, using, modifying and/or
+ * developing or reproducing the software by the user in light of its specific status of free software, that may mean
+ * that it is complicated to manipulate, and that also therefore means that it is reserved for developers and
+ * experienced professionals having in-depth computer knowledge. Users are therefore encouraged to load and test the
+ * software's suitability as regards their requirements in conditions enabling the security of their systems and/or data
+ * to be ensured and, more generally, to use and operate it in the same conditions as regards security.
+ *
+ * The fact that you are presently reading this means that you have had knowledge of the CeCILL 2.1 license and that you
+ * accept its terms.
+ *******************************************************************************/
+package fr.gouv.vitam.worker.core.handler;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.common.base.Strings;
+import fr.gouv.vitam.common.LocalDateUtil;
+import fr.gouv.vitam.common.SedaConstants;
+import fr.gouv.vitam.common.exception.InvalidParseOperationException;
+import fr.gouv.vitam.common.guid.GUID;
+import fr.gouv.vitam.common.guid.GUIDFactory;
+import fr.gouv.vitam.common.json.JsonHandler;
+import fr.gouv.vitam.common.logging.VitamLogger;
+import fr.gouv.vitam.common.logging.VitamLoggerFactory;
+import fr.gouv.vitam.common.model.ItemStatus;
+import fr.gouv.vitam.common.model.RequestResponse;
+import fr.gouv.vitam.common.model.StatusCode;
+import fr.gouv.vitam.common.model.VitamAutoCloseable;
+import fr.gouv.vitam.common.model.administration.AccessionRegisterDetailModel;
+import fr.gouv.vitam.common.model.administration.AccessionRegisterStatus;
+import fr.gouv.vitam.common.model.administration.RegisterValueDetailModel;
+import fr.gouv.vitam.common.model.administration.RegisterValueEventModel;
+import fr.gouv.vitam.common.server.HeaderIdHelper;
+import fr.gouv.vitam.functional.administration.client.AdminManagementClient;
+import fr.gouv.vitam.functional.administration.client.AdminManagementClientFactory;
+import fr.gouv.vitam.functional.administration.common.exception.AccessionRegisterException;
+import fr.gouv.vitam.functional.administration.common.exception.AdminManagementClientServerException;
+import fr.gouv.vitam.logbook.common.parameters.LogbookTypeProcess;
+import fr.gouv.vitam.metadata.api.exception.MetaDataClientServerException;
+import fr.gouv.vitam.metadata.api.model.ObjectGroupPerOriginatingAgency;
+import fr.gouv.vitam.metadata.api.model.UnitPerOriginatingAgency;
+import fr.gouv.vitam.metadata.client.MetaDataClient;
+import fr.gouv.vitam.metadata.client.MetaDataClientFactory;
+import fr.gouv.vitam.processing.common.exception.ProcessingException;
+import fr.gouv.vitam.processing.common.parameter.WorkerParameters;
+import fr.gouv.vitam.worker.common.HandlerIO;
+import fr.gouv.vitam.worker.core.impl.HandlerIOImpl;
+
+import java.io.File;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Accession Register Handler
+ */
+public class IngestAccessionRegisterActionHandler extends AbstractAccessionRegisterAction {
+    private static final VitamLogger LOGGER = VitamLoggerFactory.getInstance(IngestAccessionRegisterActionHandler.class);
+    private static final String HANDLER_ID = "ACCESSION_REGISTRATION";
+
+    /**
+     * Empty Constructor AccessionRegisterActionHandler
+     */
+    public IngestAccessionRegisterActionHandler() {
+        this(MetaDataClientFactory.getInstance(), AdminManagementClientFactory.getInstance());
+    }
+
+    IngestAccessionRegisterActionHandler(MetaDataClientFactory metaDataClientFactory,
+                                         AdminManagementClientFactory adminManagementClientFactory) {
+        super(metaDataClientFactory, adminManagementClientFactory);
+    }
+
+    @Override
+    public ItemStatus execute(WorkerParameters params, HandlerIO handler) {
+        LOGGER.debug("AbstractAccessionRegisterAction running ...");
+        return super.execute(params, handler);
+    }
+
+    @Override
+    protected String getHandlerId() {
+        return HANDLER_ID;
+    }
+
+    @Override
+    protected LogbookTypeProcess getOperationType() {
+        return LogbookTypeProcess.INGEST;
+    }
+
+    /**
+     * @return HANDLER_ID
+     */
+    public static String getId() {
+        return HANDLER_ID;
+    }
+
+}
---
+++ sources/worker/worker-core/src/test/java/fr/gouv/vitam/worker/core/plugin/preservation/PreservationAccessionRegisterActionHandlerTest.java
@@ -0,0 +1,287 @@
+/*******************************************************************************
+ * Copyright French Prime minister Office/SGMAP/DINSIC/Vitam Program (2015-2019)
+ *
+ * contact.vitam@culture.gouv.fr
+ *
+ * This software is a computer program whose purpose is to implement a digital archiving back-office system managing
+ * high volumetry securely and efficiently.
+ *
+ * This software is governed by the CeCILL 2.1 license under French law and abiding by the rules of distribution of free
+ * software. You can use, modify and/ or redistribute the software under the terms of the CeCILL 2.1 license as
+ * circulated by CEA, CNRS and INRIA at the following URL "http://www.cecill.info".
+ *
+ * As a counterpart to the access to the source code and rights to copy, modify and redistribute granted by the license,
+ * users are provided only with a limited warranty and the software's author, the holder of the economic rights, and the
+ * successive licensors have only limited liability.
+ *
+ * In this respect, the user's attention is drawn to the risks associated with loading, using, modifying and/or
+ * developing or reproducing the software by the user in light of its specific status of free software, that may mean
+ * that it is complicated to manipulate, and that also therefore means that it is reserved for developers and
+ * experienced professionals having in-depth computer knowledge. Users are therefore encouraged to load and test the
+ * software's suitability as regards their requirements in conditions enabling the security of their systems and/or data
+ * to be ensured and, more generally, to use and operate it in the same conditions as regards security.
+ *
+ * The fact that you are presently reading this means that you have had knowledge of the CeCILL 2.1 license and that you
+ * accept its terms.
+ *******************************************************************************/
+
+
+package fr.gouv.vitam.worker.core.plugin.preservation;
+
+import fr.gouv.vitam.common.PropertiesUtils;
+import fr.gouv.vitam.common.error.ServiceName;
+import fr.gouv.vitam.common.error.VitamError;
+import fr.gouv.vitam.common.guid.GUID;
+import fr.gouv.vitam.common.guid.GUIDFactory;
+import fr.gouv.vitam.common.model.ItemStatus;
+import fr.gouv.vitam.common.model.RequestResponse;
+import fr.gouv.vitam.common.model.RequestResponseOK;
+import fr.gouv.vitam.common.model.StatusCode;
+import fr.gouv.vitam.common.model.administration.AccessionRegisterDetailModel;
+import fr.gouv.vitam.common.model.processing.IOParameter;
+import fr.gouv.vitam.common.model.processing.ProcessingUri;
+import fr.gouv.vitam.common.model.processing.UriPrefix;
+import fr.gouv.vitam.common.thread.RunWithCustomExecutor;
+import fr.gouv.vitam.common.thread.RunWithCustomExecutorRule;
+import fr.gouv.vitam.common.thread.VitamThreadPoolExecutor;
+import fr.gouv.vitam.common.thread.VitamThreadUtils;
+import fr.gouv.vitam.functional.administration.client.AdminManagementClient;
+import fr.gouv.vitam.functional.administration.client.AdminManagementClientFactory;
+import fr.gouv.vitam.functional.administration.common.exception.AdminManagementClientServerException;
+import fr.gouv.vitam.metadata.api.exception.MetaDataClientServerException;
+import fr.gouv.vitam.metadata.api.model.ObjectGroupPerOriginatingAgency;
+import fr.gouv.vitam.metadata.client.MetaDataClient;
+import fr.gouv.vitam.metadata.client.MetaDataClientFactory;
+import fr.gouv.vitam.processing.common.parameter.WorkerParameters;
+import fr.gouv.vitam.processing.common.parameter.WorkerParametersFactory;
+import fr.gouv.vitam.worker.core.impl.HandlerIOImpl;
+import org.assertj.core.util.Lists;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.when;
+
+public class PreservationAccessionRegisterActionHandlerTest {
+    private static final String ATR_GLOBAL_SEDA_PARAMETERS = "globalSEDAParameters.json";
+    private static final String FAKE_URL = "http://localhost:8080";
+    PreservationAccessionRegisterActionHandler accessionRegisterHandler;
+    private static final String HANDLER_ID = "PRESERVATION_ACCESSION_REGISTRATION";
+    private HandlerIOImpl handlerIO;
+    private GUID guid;
+    private WorkerParameters params;
+    private static final Integer TENANT_ID = 0;
+
+    @Rule
+    public RunWithCustomExecutorRule runInThread =
+            new RunWithCustomExecutorRule(VitamThreadPoolExecutor.getDefaultExecutor());
+
+
+    MetaDataClient metaDataClient = mock(MetaDataClient.class);
+    MetaDataClientFactory metaDataClientFactory = mock(MetaDataClientFactory.class);
+
+    AdminManagementClient adminManagementClient = mock(AdminManagementClient.class);
+    AdminManagementClientFactory adminManagementClientFactory = mock(AdminManagementClientFactory.class);
+
+    @Before
+    public void setUp() throws Exception {
+        AdminManagementClientFactory.changeMode(null);
+        guid = GUIDFactory.newGUID();
+        params =
+                WorkerParametersFactory.newWorkerParameters().setUrlWorkspace(FAKE_URL).setUrlMetadata(FAKE_URL)
+                        .setObjectNameList(Lists.newArrayList("objectName.json")).setObjectName("objectName.json")
+                        .setCurrentStep("currentStep").setContainerName(guid.getId());
+        String objectId = "manifest";
+        handlerIO = new HandlerIOImpl(guid.getId(), "workerId", newArrayList(objectId));
+        handlerIO.setCurrentObjectId(objectId);
+
+        when(metaDataClientFactory.getClient()).thenReturn(metaDataClient);
+        when(adminManagementClientFactory.getClient()).thenReturn(adminManagementClient);
+    }
+
+    @After
+    public void end() {
+        handlerIO.partialClose();
+    }
+
+    @Test
+    @RunWithCustomExecutor
+    public void testResponseOK() throws Exception {
+        // Given
+        VitamThreadUtils.getVitamSession().setTenantId(TENANT_ID);
+        GUID operationId = GUIDFactory.newGUID();
+        VitamThreadUtils.getVitamSession().setRequestId(operationId);
+
+        params.setContainerName(operationId.getId());
+
+        List<ObjectGroupPerOriginatingAgency> originatingAgencies = new ArrayList<>();
+        originatingAgencies.add(new ObjectGroupPerOriginatingAgency(operationId.getId(), "FRAN_NP_005568", 1l, 1l, 1l));
+
+
+        reset(metaDataClient);
+        reset(adminManagementClient);
+        when(metaDataClient.selectAccessionRegisterOnObjectByOperationId(operationId.toString()))
+                .thenReturn(originatingAgencies);
+
+
+        AdminManagementClientFactory.changeMode(null);
+        handlerIO.reset();
+        accessionRegisterHandler =
+                new PreservationAccessionRegisterActionHandler(metaDataClientFactory, adminManagementClientFactory);
+        assertEquals(PreservationAccessionRegisterActionHandler.getId(), HANDLER_ID);
+
+        RequestResponse<AccessionRegisterDetailModel> res =
+                new RequestResponseOK<AccessionRegisterDetailModel>().setHttpCode(201);
+        when(adminManagementClient.createorUpdateAccessionRegister(anyObject()))
+                .thenReturn(res);
+
+        // When
+        final ItemStatus response = accessionRegisterHandler.execute(params, handlerIO);
+
+        // Then
+        assertEquals(StatusCode.OK, response.getGlobalStatus());
+    }
+
+    @Test
+    @RunWithCustomExecutor
+    public void testResponseKOCouldNotCreateRegister() throws Exception {
+        // Given
+        VitamThreadUtils.getVitamSession().setTenantId(TENANT_ID);
+        GUID operationId = GUIDFactory.newGUID();
+        VitamThreadUtils.getVitamSession().setRequestId(operationId);
+
+        params.setContainerName(operationId.getId());
+
+        List<ObjectGroupPerOriginatingAgency> originatingAgencies = new ArrayList<>();
+        originatingAgencies.add(new ObjectGroupPerOriginatingAgency(operationId.getId(), "FRAN_NP_005568", 1l, 1l, 1l));
+
+        reset(metaDataClient);
+        reset(adminManagementClient);
+        when(metaDataClient.selectAccessionRegisterOnObjectByOperationId(operationId.toString()))
+                .thenReturn(originatingAgencies);
+
+
+        when(adminManagementClient.createorUpdateAccessionRegister(anyObject()))
+                .thenThrow(new AdminManagementClientServerException("AdminManagementClientServerException"));
+
+        AdminManagementClientFactory.changeMode(null);
+        handlerIO.reset();
+        accessionRegisterHandler =
+                new PreservationAccessionRegisterActionHandler(metaDataClientFactory, adminManagementClientFactory);
+        assertEquals(PreservationAccessionRegisterActionHandler.getId(), HANDLER_ID);
+
+        // When
+        final ItemStatus response = accessionRegisterHandler.execute(params, handlerIO);
+
+        // Then
+        assertEquals(StatusCode.KO, response.getGlobalStatus());
+
+    }
+
+    @Test
+    @RunWithCustomExecutor
+    public void testResponseConflictAlreadyExecutedAccesionRegister() throws Exception {
+        // Given
+        VitamThreadUtils.getVitamSession().setTenantId(TENANT_ID);
+        GUID operationId = GUIDFactory.newGUID();
+        VitamThreadUtils.getVitamSession().setRequestId(operationId);
+
+        params.setContainerName(operationId.getId());
+
+        List<ObjectGroupPerOriginatingAgency> originatingAgencies = new ArrayList<>();
+        originatingAgencies.add(new ObjectGroupPerOriginatingAgency(operationId.getId(), "FRAN_NP_005568", 1l, 1l, 1l));
+
+        reset(metaDataClient);
+        reset(adminManagementClient);
+        when(metaDataClient.selectAccessionRegisterOnObjectByOperationId(operationId.toString()))
+                .thenReturn(originatingAgencies);
+
+        VitamError ve =
+                new VitamError(Response.Status.CONFLICT.name()).setHttpCode(Response.Status.CONFLICT.getStatusCode())
+                        .setContext(ServiceName.EXTERNAL_ACCESS.getName())
+                        .setState("code_vitam")
+                        .setMessage(Response.Status.CONFLICT.getReasonPhrase())
+                        .setDescription("Document already exists in database");
+
+        when(adminManagementClient.createorUpdateAccessionRegister(anyObject()))
+                .thenReturn(ve);
+
+        AdminManagementClientFactory.changeMode(null);
+        handlerIO.reset();
+        accessionRegisterHandler =
+                new PreservationAccessionRegisterActionHandler(metaDataClientFactory, adminManagementClientFactory);
+        assertEquals(PreservationAccessionRegisterActionHandler.getId(), HANDLER_ID);
+
+        // When
+        final ItemStatus response = accessionRegisterHandler.execute(params, handlerIO);
+
+        // Then
+        assertEquals(StatusCode.ALREADY_EXECUTED, response.getGlobalStatus());
+
+    }
+
+
+    @Test
+    @RunWithCustomExecutor
+    public void testResponseFatal() throws Exception {
+        // Given
+        VitamThreadUtils.getVitamSession().setTenantId(TENANT_ID);
+        GUID operationId = GUIDFactory.newGUID();
+        VitamThreadUtils.getVitamSession().setRequestId(operationId);
+
+        params.setContainerName(operationId.getId());
+
+        List<ObjectGroupPerOriginatingAgency> originatingAgencies = new ArrayList<>();
+        originatingAgencies.add(new ObjectGroupPerOriginatingAgency(operationId.getId(), "FRAN_NP_005568", 1l, 1l, 1l));
+
+
+        reset(metaDataClient);
+        reset(adminManagementClient);
+        when(metaDataClient.selectAccessionRegisterOnObjectByOperationId(operationId.toString()))
+                .thenThrow(new MetaDataClientServerException("MetaDataClientServerException"));
+
+        AdminManagementClientFactory.changeMode(null);
+        handlerIO.reset();
+        accessionRegisterHandler =
+                new PreservationAccessionRegisterActionHandler(metaDataClientFactory, adminManagementClientFactory);
+        assertEquals(PreservationAccessionRegisterActionHandler.getId(), HANDLER_ID);
+        // When
+        final ItemStatus response = accessionRegisterHandler.execute(params, handlerIO);
+        // Then
+        assertEquals(StatusCode.FATAL, response.getGlobalStatus());
+    }
+
+    @Test
+    @RunWithCustomExecutor
+    public void testResponseOKWithNoAgency() throws Exception {
+        // Given
+        VitamThreadUtils.getVitamSession().setTenantId(TENANT_ID);
+        GUID operationId = GUIDFactory.newGUID();
+        VitamThreadUtils.getVitamSession().setRequestId(operationId);
+        reset(metaDataClient);
+        reset(adminManagementClient);
+        params.setContainerName(operationId.getId());
+        AdminManagementClientFactory.changeMode(null);
+        handlerIO.reset();
+        accessionRegisterHandler =
+                new PreservationAccessionRegisterActionHandler(metaDataClientFactory, adminManagementClientFactory);
+        assertEquals(PreservationAccessionRegisterActionHandler.getId(), HANDLER_ID);
+
+        // When
+        final ItemStatus response = accessionRegisterHandler.execute(params, handlerIO);
+
+        // Then
+        assertEquals(StatusCode.OK, response.getGlobalStatus());
+    }
+
+}
