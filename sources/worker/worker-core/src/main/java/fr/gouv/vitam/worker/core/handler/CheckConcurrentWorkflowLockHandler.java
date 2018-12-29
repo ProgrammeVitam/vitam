f05fc218fdd4a2734e67325cbc27b6b3b3eba16c
Lotfi GHERIBI <lotfi.gheribi.ext@culture.gouv.fr>
2018-12-29 15:53:24 +0100

    Bug 5471 : Lightweight lock for unit & object group lifecycle traceability workflows

---
+++ sources/worker/worker-core/src/main/java/fr/gouv/vitam/worker/core/handler/CheckConcurrentWorkflowLockHandler.java
@@ -0,0 +1,122 @@
+/*******************************************************************************
+ * Copyright French Prime minister Office/SGMAP/DINSIC/Vitam Program (2015-2019)
+ *
+ * contact.vitam@culture.gouv.fr
+ *
+ * This software is a computer program whose purpose is to implement a digital archiving back-office system managing
+ * high volumetry securely and efficiently.
+ *
+ * This software is governed by the CeCILL 2.1 license under French law and abiding by the rules of distribution of free
+ * software. You can use, modify and/ or redistribute the software under the terms of the CeCILL 2.1 license as
+ * circulated by CEA, CNRS and INRIA at the following URL "http://www.cecill.info".
+ *
+ * As a counterpart to the access to the source code and rights to copy, modify and redistribute granted by the license,
+ * users are provided only with a limited warranty and the software's author, the holder of the economic rights, and the
+ * successive licensors have only limited liability.
+ *
+ * In this respect, the user's attention is drawn to the risks associated with loading, using, modifying and/or
+ * developing or reproducing the software by the user in light of its specific status of free software, that may mean
+ * that it is complicated to manipulate, and that also therefore means that it is reserved for developers and
+ * experienced professionals having in-depth computer knowledge. Users are therefore encouraged to load and test the
+ * software's suitability as regards their requirements in conditions enabling the security of their systems and/or data
+ * to be ensured and, more generally, to use and operate it in the same conditions as regards security.
+ *
+ * The fact that you are presently reading this means that you have had knowledge of the CeCILL 2.1 license and that you
+ * accept its terms.
+ *******************************************************************************/
+package fr.gouv.vitam.worker.core.handler;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.common.annotations.VisibleForTesting;
+import fr.gouv.vitam.common.exception.VitamClientException;
+import fr.gouv.vitam.common.json.JsonHandler;
+import fr.gouv.vitam.common.logging.VitamLogger;
+import fr.gouv.vitam.common.logging.VitamLoggerFactory;
+import fr.gouv.vitam.common.model.ItemStatus;
+import fr.gouv.vitam.common.model.StatusCode;
+import fr.gouv.vitam.common.model.processing.ProcessDetail;
+import fr.gouv.vitam.processing.common.exception.ProcessingException;
+import fr.gouv.vitam.processing.common.parameter.WorkerParameters;
+import fr.gouv.vitam.worker.common.HandlerIO;
+import fr.gouv.vitam.worker.core.utils.LightweightWorkflowLock;
+import fr.gouv.vitam.workspace.api.exception.ContentAddressableStorageServerException;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static fr.gouv.vitam.worker.core.utils.PluginHelper.buildItemStatus;
+
+/**
+ * Concurrent workflow lock check handler.
+ */
+public class CheckConcurrentWorkflowLockHandler extends ActionHandler {
+
+    private static final VitamLogger LOGGER =
+        VitamLoggerFactory.getInstance(CheckConcurrentWorkflowLockHandler.class);
+
+    private static final String CHECK_CONCURRENT_WORKFLOW_LOCK = "CHECK_CONCURRENT_WORKFLOW_LOCK";
+    private static final int WORKFLOW_IDS_RANK = 0;
+    static final String CONCURRENT_PROCESSES_FOUND = "Concurrent process(es) found";
+    private final LightweightWorkflowLock lightweightWorkflowLock;
+
+    /**
+     * Default constructor
+     */
+    public CheckConcurrentWorkflowLockHandler() {
+        this(new LightweightWorkflowLock());
+    }
+
+    /***
+     * Test only constructor
+     */
+    @VisibleForTesting
+    CheckConcurrentWorkflowLockHandler(LightweightWorkflowLock lightweightWorkflowLock) {
+        this.lightweightWorkflowLock = lightweightWorkflowLock;
+    }
+
+    @Override
+    public ItemStatus execute(WorkerParameters param, HandlerIO handler)
+        throws ProcessingException, ContentAddressableStorageServerException {
+
+        try {
+
+            String workflowIdsStr = (String) handler.getInput(WORKFLOW_IDS_RANK);
+            List<String> workflowIds = Arrays.stream(workflowIdsStr.split(","))
+                .map(String::trim)
+                .collect(Collectors.toList());
+
+            List<ProcessDetail> concurrentWorkflows = lightweightWorkflowLock
+                .listConcurrentWorkflows(workflowIds, param.getContainerName());
+
+            if (!concurrentWorkflows.isEmpty()) {
+
+                LOGGER.error("Concurrent process(es) found " +
+                    concurrentWorkflows.stream().map(
+                        i -> i.getProcessType() + " " + i.getOperationId() + "(" + i.getGlobalState() + "/" +
+                            i.getStepStatus() + ")")
+                        .collect(Collectors.joining(", ", "[", "]")));
+
+                ObjectNode eventDetails = JsonHandler.createObjectNode();
+                eventDetails.put("error", CONCURRENT_PROCESSES_FOUND);
+                return buildItemStatus(CHECK_CONCURRENT_WORKFLOW_LOCK, StatusCode.KO, eventDetails);
+            }
+
+        } catch (VitamClientException e) {
+            LOGGER.error("Concurrent workflow lock check failed", e);
+            return buildItemStatus(CHECK_CONCURRENT_WORKFLOW_LOCK, StatusCode.FATAL, null);
+        }
+
+        LOGGER.info("Concurrent workflow lock check succeeded");
+        return buildItemStatus(CHECK_CONCURRENT_WORKFLOW_LOCK, StatusCode.OK, null);
+    }
+
+    @Override
+    public void checkMandatoryIOParameter(HandlerIO handler) throws ProcessingException {
+        // NOP.
+    }
+
+    public static String getId() {
+        return CHECK_CONCURRENT_WORKFLOW_LOCK;
+    }
+}
---
+++ sources/worker/worker-core/src/main/java/fr/gouv/vitam/worker/core/utils/LightweightWorkflowLock.java
@@ -0,0 +1,109 @@
+/*******************************************************************************
+ * Copyright French Prime minister Office/SGMAP/DINSIC/Vitam Program (2015-2019)
+ *
+ * contact.vitam@culture.gouv.fr
+ *
+ * This software is a computer program whose purpose is to implement a digital archiving back-office system managing
+ * high volumetry securely and efficiently.
+ *
+ * This software is governed by the CeCILL 2.1 license under French law and abiding by the rules of distribution of free
+ * software. You can use, modify and/ or redistribute the software under the terms of the CeCILL 2.1 license as
+ * circulated by CEA, CNRS and INRIA at the following URL "http://www.cecill.info".
+ *
+ * As a counterpart to the access to the source code and rights to copy, modify and redistribute granted by the license,
+ * users are provided only with a limited warranty and the software's author, the holder of the economic rights, and the
+ * successive licensors have only limited liability.
+ *
+ * In this respect, the user's attention is drawn to the risks associated with loading, using, modifying and/or
+ * developing or reproducing the software by the user in light of its specific status of free software, that may mean
+ * that it is complicated to manipulate, and that also therefore means that it is reserved for developers and
+ * experienced professionals having in-depth computer knowledge. Users are therefore encouraged to load and test the
+ * software's suitability as regards their requirements in conditions enabling the security of their systems and/or data
+ * to be ensured and, more generally, to use and operate it in the same conditions as regards security.
+ *
+ * The fact that you are presently reading this means that you have had knowledge of the CeCILL 2.1 license and that you
+ * accept its terms.
+ *******************************************************************************/
+package fr.gouv.vitam.worker.core.utils;
+
+import com.google.common.annotations.VisibleForTesting;
+import fr.gouv.vitam.common.error.VitamError;
+import fr.gouv.vitam.common.exception.VitamClientException;
+import fr.gouv.vitam.common.model.ProcessQuery;
+import fr.gouv.vitam.common.model.ProcessState;
+import fr.gouv.vitam.common.model.RequestResponse;
+import fr.gouv.vitam.common.model.RequestResponseOK;
+import fr.gouv.vitam.common.model.StatusCode;
+import fr.gouv.vitam.common.model.processing.ProcessDetail;
+import fr.gouv.vitam.processing.management.client.ProcessingManagementClient;
+import fr.gouv.vitam.processing.management.client.ProcessingManagementClientFactory;
+import fr.gouv.vitam.worker.core.plugin.reclassification.exception.ReclassificationException;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Helper class for checking concurrent non completed (running / paused) workflows.
+ *
+ * Provides lightweight lock implementation (at most one process can run in parallel).
+ */
+public class LightweightWorkflowLock {
+
+    private final ProcessingManagementClientFactory processingManagementClientFactory;
+
+    /**
+     * Default constructor
+     */
+    public LightweightWorkflowLock() {
+        this(ProcessingManagementClientFactory.getInstance());
+    }
+
+    /***
+     * Test only constructor
+     */
+    @VisibleForTesting
+    public LightweightWorkflowLock(ProcessingManagementClientFactory processingManagementClientFactory) {
+        this.processingManagementClientFactory = processingManagementClientFactory;
+    }
+
+    /**
+     * Returns all concurrent non completed (running / paused) workflows.
+     *
+     * @param workflowIds the workflow Ids to check
+     * @param currentProcessId the current process id (filtred from result)
+     * @return the list of concurrent workflows if any, or an empty list if no concurrent workflow is found.
+     */
+    public List<ProcessDetail> listConcurrentWorkflows(List<String> workflowIds, String currentProcessId)
+        throws VitamClientException {
+
+        try (ProcessingManagementClient client = processingManagementClientFactory.getClient()) {
+
+            ProcessQuery query = new ProcessQuery();
+            // Non completed processes
+            query.setStates(
+                Arrays.stream(ProcessState.values())
+                    .filter(state -> state.compareTo(ProcessState.COMPLETED) < 0)
+                    .map(Enum::name)
+                    .collect(Collectors.toList())
+            );
+            // Workflow id
+            query.setWorkflows(workflowIds);
+
+            RequestResponse<ProcessDetail> processDetailRequestResponse = client.listOperationsDetails(query);
+            if (!processDetailRequestResponse.isOk()) {
+
+                VitamError error = (VitamError) processDetailRequestResponse;
+                throw new VitamClientException("Could not check concurrent workflows " + error.getDescription() + " - " + error.getMessage());
+            }
+
+            List<ProcessDetail> processDetails =
+                ((RequestResponseOK<ProcessDetail>) processDetailRequestResponse).getResults();
+
+            return processDetails.stream()
+                .filter(processDetail -> !currentProcessId.equals(processDetail.getOperationId()))
+                .collect(Collectors.toList());
+        }
+    }
+}
