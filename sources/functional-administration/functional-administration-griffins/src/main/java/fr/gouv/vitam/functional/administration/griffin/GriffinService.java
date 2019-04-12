18adce56ec8d0462ec2842f5b7778ac4d5d52227
rocha <romain.chauveau@smile.fr>
2019-04-12 12:06:05 +0200

    fix for bug 5572

--- sources/functional-administration/functional-administration-griffins/src/main/java/fr/gouv/vitam/functional/administration/griffin/GriffinService.java
+++ sources/functional-administration/functional-administration-griffins/src/main/java/fr/gouv/vitam/functional/administration/griffin/GriffinService.java
@@ -330,6 +330,7 @@ public class GriffinService {
         List<String> usedGriffinsIds = allScenariosInDatabase
             .stream()
             .flatMap(preservationScenarioModel -> preservationScenarioModel.getGriffinByFormat().stream())
+
             .map(GriffinByFormat::getGriffinIdentifier)
             .collect(Collectors.toList());
 
--- sources/functional-administration/functional-administration-griffins/src/test/java/fr/gouv/vitam/functional/administration/griffin/GriffinServiceTest.java
+++ sources/functional-administration/functional-administration-griffins/src/test/java/fr/gouv/vitam/functional/administration/griffin/GriffinServiceTest.java
@@ -271,7 +271,6 @@ public class GriffinServiceTest {
     @RunWithCustomExecutor
     public void givenRemovingUsedGriffinShouldFailedImport() throws Exception {
         List<GriffinModel> allGriffinInDatabase = getGriffinsModels("griffins_referentiel.json");
-
         List<PreservationScenarioModel> allPreservationScenarioInDatabase =
             getPreservationScenarioModels("preservation_scenario.json");
 
@@ -298,9 +297,6 @@ public class GriffinServiceTest {
         when(mongoDbAccess.findDocuments(any(JsonNode.class), eq(PRESERVATION_SCENARIO))).thenReturn(dbRequestResult);
         when(dbRequestResult.getDocuments(FileFormat.class, FileFormatModel.class)).thenReturn(listFormat);
         when(mongoDbAccess.findDocuments(any(JsonNode.class), eq(FORMATS))).thenReturn(dbRequestResult);
-
-        List<PreservationScenarioModel> listPreservationScenarioToImport = getPreservationScenarioModels("preservation_scenario.json");
-        preservationScenarioService.importScenarios(listPreservationScenarioToImport);
 
         List<GriffinModel> listGriffinsToImport = getGriffinsModels("griffins/KO_griffin_maj_remove_used_griffin.json");
         assertThatThrownBy(() -> griffinService.importGriffin(listGriffinsToImport))
