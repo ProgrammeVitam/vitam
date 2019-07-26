3c9d9f8a8d15a125cea800e78d7e095066fab018
Djamel HAMAS <djamel.hamas.ext@culture.gouv.fr>
2019-07-26 13:28:11 +0200

    Change read request database model

--- sources/common/common-public/src/main/java/fr/gouv/vitam/common/model/tape/FileInTape.java
+++ sources/common/common-public/src/main/java/fr/gouv/vitam/common/model/tape/FileInTape.java
@@ -6,18 +6,29 @@ import java.util.List;
 
 public class FileInTape {
 
-    @JsonProperty("fileName")
+    public static final String FILE_NAME = "fileName";
+    public static final String STORAGE_ID = "storageId";
+    public static final String FILE_SEGMENTS = "fileSegments";
+
+    @JsonProperty(FILE_NAME)
     private String fileName;
 
-    @JsonProperty("fileSegments")
+    @JsonProperty(STORAGE_ID)
+    private String storageId;
+
+
+    @JsonProperty(FILE_SEGMENTS)
     private List<TarEntryDescription> fileSegments;
 
     public FileInTape() {
         // Empty constructor for deserialization
     }
 
-    public FileInTape(String fileName, List<TarEntryDescription> fileSegments) {
+
+    public FileInTape(String fileName, String storageId,
+        List<TarEntryDescription> fileSegments) {
         this.fileName = fileName;
+        this.storageId = storageId;
         this.fileSegments = fileSegments;
     }
 
@@ -27,6 +38,14 @@ public class FileInTape {
 
     public void setFileName(String fileName) {
         this.fileName = fileName;
+    }
+
+    public String getStorageId() {
+        return storageId;
+    }
+
+    public void setStorageId(String storageId) {
+        this.storageId = storageId;
     }
 
     public List<TarEntryDescription> getFileSegments() {
--- sources/common/common-public/src/main/java/fr/gouv/vitam/common/model/tape/TapeReadRequestReferentialEntity.java
+++ sources/common/common-public/src/main/java/fr/gouv/vitam/common/model/tape/TapeReadRequestReferentialEntity.java
@@ -4,23 +4,26 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
+import java.util.Map;
 
+// MongoDB doc limit is 16Mo => should define the adequate bulk size (read threshold)
 public class TapeReadRequestReferentialEntity {
     public static final String ID = "_id";
-    public static final String EXPECTED_TARS_COUNT = "expectedTarsCount";
-    public static final String CURRENT_READ_TARS_COUNT = "currentReadTarsCount";
+    public static final String TAR_LOCATIONS = "tarLocation";
     public static final String FILES = "files";
+    public static final String CONTAINER_NAME = "containerName";
 
     @JsonProperty(ID)
     private String requestId;
 
-    @JsonProperty(EXPECTED_TARS_COUNT)
-    private int expectedTarsCount;
+    @JsonProperty(CONTAINER_NAME)
+    private String containerName;
 
-    @JsonProperty(CURRENT_READ_TARS_COUNT)
-    private int currentReadTarsCount = 0;
+    // Map of tar to tar location
+    @JsonProperty(TAR_LOCATIONS)
+    private Map<String, TarLocation> tarLocations;
 
-    // MongoDB doc limit is 16Mo => should define the adequate bulk size (read threshold)
+
     @JsonProperty(FILES)
     private List<FileInTape> files;
 
@@ -28,22 +31,49 @@ public class TapeReadRequestReferentialEntity {
         // Empty constructor for deserialization
     }
 
-    public TapeReadRequestReferentialEntity(String requestId, int expectedTarsCount, List<FileInTape> files) {
+    public TapeReadRequestReferentialEntity(String requestId, String containerName,
+        Map<String, TarLocation> tarLocations, List<FileInTape> files) {
         this.requestId = requestId;
-        this.expectedTarsCount = expectedTarsCount;
+        this.containerName = containerName;
+        this.tarLocations = tarLocations;
         this.files = files;
     }
 
+
     public String getRequestId() {
         return requestId;
+    }
+
+    public void setRequestId(String requestId) {
+        this.requestId = requestId;
+    }
+
+    public String getContainerName() {
+        return containerName;
+    }
+
+    public void setContainerName(String containerName) {
+        this.containerName = containerName;
+    }
+
+    public Map<String, TarLocation> getTarLocations() {
+        return tarLocations;
+    }
+
+    public void setTarLocations(Map<String, TarLocation> tarLocations) {
+        this.tarLocations = tarLocations;
     }
 
     public List<FileInTape> getFiles() {
         return files;
     }
 
+    public void setFiles(List<FileInTape> files) {
+        this.files = files;
+    }
+
     @JsonIgnore
     public boolean isCompleted() {
-        return expectedTarsCount == currentReadTarsCount;
+        return tarLocations.values().stream().filter(o -> TarLocation.DISK.equals(o)).count() == tarLocations.size();
     }
 }
