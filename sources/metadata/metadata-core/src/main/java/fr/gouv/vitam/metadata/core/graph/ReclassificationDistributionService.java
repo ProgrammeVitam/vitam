b27fee625b5454ba00d2c587a9308db2ab9a64d1
Lotfi Gheribi <lotfi.gheribi.ext@culture.gouv.fr>
2019-08-29 20:40:19 +0200

    Bug 6003 : Fix full scan on reclassification workflows using DSL instead on querying non indexed mongodb field "_us"

--- sources/metadata/metadata-core/src/main/java/fr/gouv/vitam/metadata/core/graph/ReclassificationDistributionService.java
+++ sources/metadata/metadata-core/src/main/java/fr/gouv/vitam/metadata/core/graph/ReclassificationDistributionService.java
@@ -26,30 +26,39 @@
  *******************************************************************************/
 package fr.gouv.vitam.metadata.core.graph;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.google.common.annotations.VisibleForTesting;
-import com.mongodb.client.FindIterable;
-import com.mongodb.client.MongoCursor;
-import com.mongodb.client.model.Filters;
-import com.mongodb.client.model.Projections;
 import fr.gouv.vitam.common.VitamConfiguration;
-import fr.gouv.vitam.common.database.api.VitamRepositoryProvider;
-import fr.gouv.vitam.common.database.api.impl.VitamMongoRepository;
+import fr.gouv.vitam.common.database.builder.query.BooleanQuery;
+import fr.gouv.vitam.common.database.builder.query.InQuery;
+import fr.gouv.vitam.common.database.builder.query.QueryHelper;
+import fr.gouv.vitam.common.database.builder.query.VitamFieldsHelper;
+import fr.gouv.vitam.common.database.builder.request.exception.InvalidCreateOperationException;
+import fr.gouv.vitam.common.database.builder.request.multiple.SelectMultiQuery;
+import fr.gouv.vitam.common.database.utils.ScrollSpliterator;
+import fr.gouv.vitam.common.exception.BadRequestException;
+import fr.gouv.vitam.common.exception.InvalidParseOperationException;
+import fr.gouv.vitam.common.exception.VitamDBException;
+import fr.gouv.vitam.common.iterables.SpliteratorIterator;
 import fr.gouv.vitam.common.thread.VitamThreadUtils;
-import fr.gouv.vitam.metadata.core.database.collections.MetadataCollections;
-import fr.gouv.vitam.metadata.core.database.collections.Unit;
+import fr.gouv.vitam.metadata.api.exception.MetaDataDocumentSizeException;
+import fr.gouv.vitam.metadata.api.exception.MetaDataExecutionException;
+import fr.gouv.vitam.metadata.api.exception.MetaDataNotFoundException;
+import fr.gouv.vitam.metadata.core.MetaDataImpl;
+import fr.gouv.vitam.metadata.core.database.configuration.GlobalDatasDb;
 import fr.gouv.vitam.worker.core.distribution.JsonLineModel;
 import fr.gouv.vitam.worker.core.distribution.JsonLineWriter;
 import fr.gouv.vitam.workspace.api.exception.ContentAddressableStorageServerException;
 import fr.gouv.vitam.workspace.client.WorkspaceClient;
 import fr.gouv.vitam.workspace.client.WorkspaceClientFactory;
 import org.apache.commons.io.FileUtils;
-import org.bson.Document;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Iterator;
 import java.util.Set;
 
 /**
@@ -61,35 +70,27 @@ public class ReclassificationDistributionService {
     private static final String UNIT_LIST_TMP_FILE_PREFIX = "unit_export_";
     private static final String OBJECT_GROUP_LIST_TMP_FILE_PREFIX = "object_group_export_";
     private final WorkspaceClientFactory workspaceClientFactory;
-    private final VitamRepositoryProvider vitamRepositoryProvider;
+    private final MetaDataImpl metaData;
 
-
-    public ReclassificationDistributionService(VitamRepositoryProvider vitamRepositoryProvider) {
+    public ReclassificationDistributionService(MetaDataImpl metaData) {
         this(WorkspaceClientFactory.getInstance(),
-            vitamRepositoryProvider);
+            metaData);
     }
 
     @VisibleForTesting
     ReclassificationDistributionService(
         WorkspaceClientFactory workspaceClientFactory,
-        VitamRepositoryProvider vitamRepositoryProvider) {
+        MetaDataImpl metaData) {
         this.workspaceClientFactory = workspaceClientFactory;
-        this.vitamRepositoryProvider = vitamRepositoryProvider;
+        this.metaData = metaData;
     }
 
-
     public void exportReclassificationChildNodes(Set<String> unitIds, String unitsToUpdateJsonLineFileName,
-        String objectGroupsToUpdateJsonLineFileName) throws IOException {
+        String objectGroupsToUpdateJsonLineFileName)
+        throws IOException, InvalidParseOperationException, InvalidCreateOperationException {
 
-        VitamMongoRepository vitamMongoRepository =
-            vitamRepositoryProvider.getVitamMongoRepository(MetadataCollections.UNIT.getVitamCollection());
-
-        FindIterable<Document> query = vitamMongoRepository.findDocuments(
-            Filters.or(
-                Filters.in(Unit.ID, unitIds),
-                Filters.in(Unit.UNITUPS, unitIds))
-            , VitamConfiguration.getBatchSize())
-            .projection(Projections.include(Unit.ID, Unit.OG));
+        SelectMultiQuery select = createSelectRequest(unitIds);
+        Iterator<JsonNode> iterator = executeSelectAsIterator(select);
 
         File tempUnitReportFile = null;
         File tempObjectGroupReportFile = null;
@@ -101,22 +102,10 @@ public class ReclassificationDistributionService {
             tempObjectGroupReportFile = File.createTempFile(OBJECT_GROUP_LIST_TMP_FILE_PREFIX, JSONL_EXTENSION,
                 new File(VitamConfiguration.getVitamTmpFolder()));
 
-            try (MongoCursor<Document> iterator = query.iterator();
-                JsonLineWriter unitReportWriter = new JsonLineWriter(new FileOutputStream(tempUnitReportFile));
+            try (JsonLineWriter unitReportWriter = new JsonLineWriter(new FileOutputStream(tempUnitReportFile));
                 JsonLineWriter objectGroupReportWriter = new JsonLineWriter(
                     new FileOutputStream(tempObjectGroupReportFile))) {
-
-                while (iterator.hasNext()) {
-                    Document doc = iterator.next();
-
-                    String id = doc.get(Unit.ID, String.class);
-                    String objectGroupId = doc.get(Unit.OG, String.class);
-
-                    unitReportWriter.addEntry(new JsonLineModel(id));
-                    if (objectGroupId != null) {
-                        objectGroupReportWriter.addEntry(new JsonLineModel(objectGroupId));
-                    }
-                }
+                appendEntries(iterator, unitReportWriter, objectGroupReportWriter);
             }
 
             storeIntoWorkspace(unitsToUpdateJsonLineFileName, tempUnitReportFile);
@@ -125,6 +114,63 @@ public class ReclassificationDistributionService {
         } finally {
             FileUtils.deleteQuietly(tempUnitReportFile);
             FileUtils.deleteQuietly(tempObjectGroupReportFile);
+        }
+    }
+
+    private SelectMultiQuery createSelectRequest(Set<String> unitIds)
+        throws InvalidCreateOperationException, InvalidParseOperationException {
+
+        SelectMultiQuery select = new SelectMultiQuery();
+
+        // Select units
+        String[] parentUnitIds = unitIds.toArray(new String[0]);
+        InQuery childrenUnitsQuery = QueryHelper.in(VitamFieldsHelper.allunitups(), parentUnitIds);
+        InQuery parentsUnitsQuery = QueryHelper.in(VitamFieldsHelper.id(), parentUnitIds);
+        BooleanQuery parentsAndProgenyUnits = QueryHelper.or().add(childrenUnitsQuery, parentsUnitsQuery);
+        select.setQuery(parentsAndProgenyUnits);
+
+        // Order by : object group Id (to be able to detect / skip duplicates)
+        select.addOrderByAscFilter(VitamFieldsHelper.object());
+
+        // Projection
+        select.addUsedProjection(VitamFieldsHelper.id(), VitamFieldsHelper.object());
+
+        return select;
+    }
+
+    private Iterator<JsonNode> executeSelectAsIterator(SelectMultiQuery select) {
+        ScrollSpliterator<JsonNode> scrollRequest = new ScrollSpliterator<>(select,
+            query -> {
+                try {
+                    return metaData.selectUnitsByQuery(query.getFinalSelect());
+                } catch (MetaDataExecutionException | MetaDataDocumentSizeException | InvalidParseOperationException | VitamDBException | BadRequestException | MetaDataNotFoundException e) {
+                    throw new IllegalStateException(e);
+                }
+            }, GlobalDatasDb.DEFAULT_SCROLL_TIMEOUT, GlobalDatasDb.LIMIT_LOAD);
+
+        return new SpliteratorIterator<>(scrollRequest);
+    }
+
+    private void appendEntries(Iterator<JsonNode> iterator, JsonLineWriter unitReportWriter,
+        JsonLineWriter objectGroupReportWriter) throws IOException {
+
+        // Used to avoid duplicates
+        String lastObjectGroupId = null;
+
+        while (iterator.hasNext()) {
+            JsonNode doc = iterator.next();
+
+            String id = doc.get(VitamFieldsHelper.id()).textValue();
+            String objectGroupId =
+                doc.has(VitamFieldsHelper.object()) ? doc.get(VitamFieldsHelper.object()).textValue() : null;
+
+            unitReportWriter.addEntry(new JsonLineModel(id));
+
+            // Only report object group Id if not duplicate
+            if (objectGroupId != null && !objectGroupId.equals(lastObjectGroupId)) {
+                objectGroupReportWriter.addEntry(new JsonLineModel(objectGroupId));
+            }
+            lastObjectGroupId = objectGroupId;
         }
     }
 
