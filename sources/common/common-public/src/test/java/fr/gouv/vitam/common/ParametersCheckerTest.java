1d3264d910ec9721e56a88b521c75f82ecb3cca2
Romain Chauveau <romain.chauveau.ext@culture.gouv.fr>
2019-09-06 11:48:20 +0200

    item 5854 - fixing RSSI issue

--- sources/common/common-public/src/test/java/fr/gouv/vitam/common/ParametersCheckerTest.java
+++ sources/common/common-public/src/test/java/fr/gouv/vitam/common/ParametersCheckerTest.java
@@ -1,4 +1,4 @@
-/*******************************************************************************
+/*
  * Copyright French Prime minister Office/SGMAP/DINSIC/Vitam Program (2015-2019)
  *
  * contact.vitam@culture.gouv.fr
@@ -23,15 +23,15 @@
  *
  * The fact that you are presently reading this means that you have had knowledge of the CeCILL 2.1 license and that you
  * accept its terms.
- *******************************************************************************/
+ */
 package fr.gouv.vitam.common;
 
-import static org.junit.Assert.fail;
+import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.Test;
+import static org.junit.Assert.fail;
 
 /**
  * Parameters Checker Test
@@ -157,5 +157,4 @@ public class ParametersCheckerTest {
         ParametersChecker.checkValue("test", 1, 1);
         ParametersChecker.checkValue("test", 1, 0);
     }
-
 }
