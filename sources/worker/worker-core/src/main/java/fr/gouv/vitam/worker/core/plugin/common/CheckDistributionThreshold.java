6c7a20d4beb4e14d7e2495c0caaf83e7c77ea402
nabel <nabil.elhajjioui@smile.fr>
2019-12-24 14:24:04 +0100

    Refacto and test

--- sources/worker/worker-core/src/main/java/fr/gouv/vitam/worker/core/plugin/common/CheckDistributionThreshold.java
+++ sources/worker/worker-core/src/main/java/fr/gouv/vitam/worker/core/plugin/common/CheckDistributionThreshold.java
@@ -1,51 +1,50 @@
-/*
+/**
  * Copyright French Prime minister Office/SGMAP/DINSIC/Vitam Program (2015-2019)
- *
+ * <p>
  * contact.vitam@culture.gouv.fr
- *
+ * <p>
  * This software is a computer program whose purpose is to implement a digital archiving back-office system managing
  * high volumetry securely and efficiently.
- *
+ * <p>
  * This software is governed by the CeCILL 2.1 license under French law and abiding by the rules of distribution of free
  * software. You can use, modify and/ or redistribute the software under the terms of the CeCILL 2.1 license as
  * circulated by CEA, CNRS and INRIA at the following URL "http://www.cecill.info".
- *
+ * <p>
  * As a counterpart to the access to the source code and rights to copy, modify and redistribute granted by the license,
  * users are provided only with a limited warranty and the software's author, the holder of the economic rights, and the
  * successive licensors have only limited liability.
- *
+ * <p>
  * In this respect, the user's attention is drawn to the risks associated with loading, using, modifying and/or
  * developing or reproducing the software by the user in light of its specific status of free software, that may mean
  * that it is complicated to manipulate, and that also therefore means that it is reserved for developers and
  * experienced professionals having in-depth computer knowledge. Users are therefore encouraged to load and test the
  * software's suitability as regards their requirements in conditions enabling the security of their systems and/or data
  * to be ensured and, more generally, to use and operate it in the same conditions as regards security.
- *
+ * <p>
  * The fact that you are presently reading this means that you have had knowledge of the CeCILL 2.1 license and that you
  * accept its terms.
  */
-package fr.gouv.vitam.worker.core.plugin.computeinheritedrules;
+package fr.gouv.vitam.worker.core.plugin.common;
 
+import com.google.common.annotations.VisibleForTesting;
 import fr.gouv.vitam.common.VitamConfiguration;
 import fr.gouv.vitam.common.model.ItemStatus;
 import fr.gouv.vitam.metadata.client.MetaDataClientFactory;
 import fr.gouv.vitam.processing.common.exception.ProcessingException;
 import fr.gouv.vitam.processing.common.parameter.WorkerParameters;
 import fr.gouv.vitam.worker.common.HandlerIO;
-import fr.gouv.vitam.worker.core.plugin.common.CheckDistributionThresholdBase;
 import fr.gouv.vitam.workspace.api.exception.ContentAddressableStorageServerException;
 
+public class CheckDistributionThreshold extends CheckDistributionThresholdBase {
 
-public class ComputedInheritedRulesCheckDistributionThreshold extends CheckDistributionThresholdBase {
+    private static final String CHECK_DISTRIBUTION_THRESHOLD = "CHECK_DISTRIBUTION_THRESHOLD";
 
-    private static final String COMPUTED_INHERITED_RULES_CHECK_DISTRIBUTION_THRESHOLD =
-        "COMPUTED_INHERITED_RULES_CHECK_DISTRIBUTION_THRESHOLD";
-
-    public ComputedInheritedRulesCheckDistributionThreshold() {
+    public CheckDistributionThreshold() {
         super(MetaDataClientFactory.getInstance());
     }
 
-    public ComputedInheritedRulesCheckDistributionThreshold(
+    @VisibleForTesting
+    public CheckDistributionThreshold(
         MetaDataClientFactory metaDataClientFactory) {
         super(metaDataClientFactory);
     }
@@ -53,11 +52,7 @@ public class ComputedInheritedRulesCheckDistributionThreshold extends CheckDistr
     @Override
     public ItemStatus execute(WorkerParameters param, HandlerIO handler)
         throws ProcessingException, ContentAddressableStorageServerException {
-        return checkThreshold(handler, VitamConfiguration.getComputedInheritedRulesThreshold(),
-            COMPUTED_INHERITED_RULES_CHECK_DISTRIBUTION_THRESHOLD);
-    }
-
-    public static String getId() {
-        return COMPUTED_INHERITED_RULES_CHECK_DISTRIBUTION_THRESHOLD;
+        return checkThreshold(handler, VitamConfiguration.getDistributionThreshold(),
+            CHECK_DISTRIBUTION_THRESHOLD);
     }
 }
