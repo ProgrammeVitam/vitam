f80af023e34a73d40c44fb67d60a24910d4b85a6
Djamel HAMAS <djamel.hamas.ext@culture.gouv.fr>
2020-02-21 15:42:00 +0100

    Bump ES to 7.6.0 version

--- deb/vitam-product/build-elasticsearch-icu.sh
+++ deb/vitam-product/build-elasticsearch-icu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ICU_VERSION="7.5.2"
+ICU_VERSION="7.6.0"
 WORKING_FOLDER=$(dirname $0)
 
 if [ ! -d ${WORKING_FOLDER}/target ]; then
--- rpm/vitam-product/vitam-elasticsearch-data/rpmbuild/SPECS/vitam-elasticsearch-data.spec
+++ rpm/vitam-product/vitam-elasticsearch-data/rpmbuild/SPECS/vitam-elasticsearch-data.spec
@@ -1,4 +1,4 @@
-%define version 7.5.2
+%define version 7.6.0
 
 Name:          vitam-elasticsearch-data
 Version:       %{version}
