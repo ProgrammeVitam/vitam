bb7a678d90f51d18aacdd6ce80cb76ea347f8950
Lotfi GHERIBI <lotfi.gheribi.ext@culture.gouv.fr>
2020-10-27 10:22:01 +0100

    Item 7128 : Hold rule DIP/Transfer export

--- sources/common/common-private/src/test/java/fr/gouv/vitam/common/mapping/dip/RuleMapperTest.java
+++ sources/common/common-private/src/test/java/fr/gouv/vitam/common/mapping/dip/RuleMapperTest.java
@@ -29,11 +29,18 @@ package fr.gouv.vitam.common.mapping.dip;
 import com.google.common.collect.Lists;
 import fr.gouv.culture.archivesdefrance.seda.v2.AccessRuleType;
 import fr.gouv.culture.archivesdefrance.seda.v2.DisseminationRuleType;
+import fr.gouv.culture.archivesdefrance.seda.v2.HoldRuleType;
 import fr.gouv.culture.archivesdefrance.seda.v2.RuleIdType;
 import fr.gouv.culture.archivesdefrance.seda.v2.StorageRuleType;
+import fr.gouv.vitam.common.SedaConstants;
 import fr.gouv.vitam.common.model.unit.RuleCategoryModel;
 import fr.gouv.vitam.common.model.unit.RuleModel;
 import org.junit.Test;
+
+import javax.xml.bind.JAXBElement;
+import javax.xml.datatype.XMLGregorianCalendar;
+import java.util.Arrays;
+import java.util.Collections;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -54,11 +61,11 @@ public class RuleMapperTest {
         // Then
         assertThat(accessRuleType.getRefNonRuleId()).extracting("value").containsExactly("AC-00021", "AC-00022");
         assertThat(accessRuleType.isPreventInheritance()).isNull();
-        assertThat(((RuleIdType)accessRuleType.getRuleAndStartDate().get(0)).getValue()).isEqualTo("AC-00023");
+        assertThat(((RuleIdType) accessRuleType.getRuleAndStartDate().get(0)).getValue()).isEqualTo("AC-00023");
         assertThat(accessRuleType.getRuleAndStartDate().get(1).toString()).isEqualTo("2017-04-01");
     }
 
-    
+
     @Test
     public void should_map_empty_rule() throws Exception {
         // Given
@@ -88,7 +95,7 @@ public class RuleMapperTest {
         // Then
         assertThat(accessRuleType.getRefNonRuleId()).extracting("value").containsExactly("AC-00021", "AC-00022");
         assertThat(accessRuleType.isPreventInheritance()).isNull();
-        assertThat(((RuleIdType)accessRuleType.getRuleAndStartDate().get(0)).getValue()).isEqualTo("AC-00023");
+        assertThat(((RuleIdType) accessRuleType.getRuleAndStartDate().get(0)).getValue()).isEqualTo("AC-00023");
         assertThat(accessRuleType.getRuleAndStartDate()).hasSize(1);
     }
 
@@ -105,9 +112,10 @@ public class RuleMapperTest {
         DisseminationRuleType disseminationRuleType = ruleMapper.fillCommonRule(ruleModel, DisseminationRuleType::new);
 
         // Then
-        assertThat(disseminationRuleType.getRefNonRuleId()).extracting("value").containsExactly("DIS-0000X", "DIS-0000Y");
+        assertThat(disseminationRuleType.getRefNonRuleId()).extracting("value")
+            .containsExactly("DIS-0000X", "DIS-0000Y");
         assertThat(disseminationRuleType.isPreventInheritance()).isNull();
-        assertThat(((RuleIdType)disseminationRuleType.getRuleAndStartDate().get(0)).getValue()).isEqualTo("RX");
+        assertThat(((RuleIdType) disseminationRuleType.getRuleAndStartDate().get(0)).getValue()).isEqualTo("RX");
         assertThat(disseminationRuleType.getRuleAndStartDate()).hasSize(1);
     }
 
@@ -126,7 +134,144 @@ public class RuleMapperTest {
         // Then
         assertThat(disseminationRuleType.getRefNonRuleId()).extracting("value").asList().isEmpty();
         assertThat(disseminationRuleType.isPreventInheritance()).isFalse();
-        assertThat(((RuleIdType)disseminationRuleType.getRuleAndStartDate().get(0)).getValue()).isEqualTo("RX");
+        assertThat(((RuleIdType) disseminationRuleType.getRuleAndStartDate().get(0)).getValue()).isEqualTo("RX");
         assertThat(disseminationRuleType.getRuleAndStartDate()).hasSize(1);
+    }
+
+    @Test
+    public void should_map_hold_rule() throws Exception {
+        // Given
+        RuleMapper ruleMapper = new RuleMapper();
+
+        RuleCategoryModel ruleCategoryModel = new RuleCategoryModel();
+
+        RuleModel ruleModel1 = new RuleModel();
+        ruleModel1.setRule("HOL-00001");
+        ruleModel1.setStartDate("2001-12-23");
+        ruleModel1.setEndDate("2011-12-23");
+        ruleModel1.setHoldReason("Reason");
+        ruleModel1.setHoldReassessingDate("2021-12-23");
+
+        RuleModel ruleModel2 = new RuleModel();
+        ruleModel2.setRule("HOL-00002");
+        ruleModel2.setHoldEndDate("2021-12-23");
+        ruleModel2.setHoldOwner("Owner");
+        ruleModel2.setPreventRearrangement(true);
+
+        RuleModel ruleModel3 = new RuleModel();
+        ruleModel3.setRule("HOL-00003");
+
+        ruleCategoryModel.getRules().add(ruleModel1);
+        ruleCategoryModel.getRules().add(ruleModel2);
+        ruleCategoryModel.getRules().add(ruleModel3);
+        ruleCategoryModel.setPreventInheritance(null);
+        ruleCategoryModel.addAllPreventRulesId(Collections.emptyList());
+
+        // When
+        HoldRuleType accessRuleType = ruleMapper.fillHoldRule(ruleCategoryModel);
+
+        // Then
+        checkRuleId(accessRuleType.getHoldRuleDefGroup().get(0), "HOL-00001");
+        checkStartDate(accessRuleType.getHoldRuleDefGroup().get(1), "2001-12-23");
+        checkHoldReassessingDate(accessRuleType.getHoldRuleDefGroup().get(2), "2021-12-23");
+        checkHoldReason(accessRuleType.getHoldRuleDefGroup().get(3), "Reason");
+
+        checkRuleId(accessRuleType.getHoldRuleDefGroup().get(4), "HOL-00002");
+        checkHoldEndDate(accessRuleType.getHoldRuleDefGroup().get(5), "2021-12-23");
+        checkHoldOwner(accessRuleType.getHoldRuleDefGroup().get(6), "Owner");
+        checkPreventRearrangement(accessRuleType.getHoldRuleDefGroup().get(7), true);
+
+        checkRuleId(accessRuleType.getHoldRuleDefGroup().get(8), "HOL-00003");
+
+        // No more entries
+        assertThat(accessRuleType.getHoldRuleDefGroup()).hasSize(9);
+
+        assertThat(accessRuleType.getRefNonRuleId()).isEmpty();
+        assertThat(accessRuleType.isPreventInheritance()).isNull();
+    }
+
+    @Test
+    public void should_map_hold_rule_ref_non_rule_ids() throws Exception {
+        // Given
+        RuleMapper ruleMapper = new RuleMapper();
+
+        RuleCategoryModel ruleCategoryModel = new RuleCategoryModel();
+
+        RuleModel ruleModel1 = new RuleModel();
+        ruleModel1.setRule("HOL-00001");
+
+        ruleCategoryModel.getRules().add(ruleModel1);
+        ruleCategoryModel.setPreventInheritance(null);
+        ruleCategoryModel.addAllPreventRulesId(Arrays.asList("HOL-00002", "HOL-00003"));
+
+        // When
+        HoldRuleType accessRuleType = ruleMapper.fillHoldRule(ruleCategoryModel);
+
+        // Then
+        checkRuleId(accessRuleType.getHoldRuleDefGroup().get(0), "HOL-00001");
+        // No more entries
+        assertThat(accessRuleType.getHoldRuleDefGroup()).hasSize(1);
+
+        assertThat(accessRuleType.getRefNonRuleId()).hasSize(2);
+        assertThat(accessRuleType.getRefNonRuleId().get(0).getValue()).isEqualTo("HOL-00002");
+        assertThat(accessRuleType.getRefNonRuleId().get(1).getValue()).isEqualTo("HOL-00003");
+        assertThat(accessRuleType.isPreventInheritance()).isNull();
+    }
+
+    @Test
+    public void should_map_hold_rule_prevent_inheritance() throws Exception {
+        // Given
+        RuleMapper ruleMapper = new RuleMapper();
+
+        RuleCategoryModel ruleCategoryModel = new RuleCategoryModel();
+        ruleCategoryModel.setPreventInheritance(true);
+        ruleCategoryModel.addAllPreventRulesId(Collections.emptyList());
+
+        // When
+        HoldRuleType accessRuleType = ruleMapper.fillHoldRule(ruleCategoryModel);
+
+        // Then
+        assertThat(accessRuleType.getHoldRuleDefGroup()).isEmpty();
+        assertThat(accessRuleType.getRefNonRuleId()).isEmpty();
+        assertThat(accessRuleType.isPreventInheritance()).isEqualTo(true);
+    }
+
+    private void checkRuleId(JAXBElement<?> jaxbElement, String ruleId) {
+        assertThat(jaxbElement.getName().getLocalPart()).isEqualTo(SedaConstants.TAG_RULE_RULE);
+        assertThat(((RuleIdType) jaxbElement.getValue()).getValue()).isEqualTo(ruleId);
+    }
+
+    private void checkStartDate(JAXBElement<?> jaxbElement, String startDate) {
+        checkDate(jaxbElement, startDate, SedaConstants.TAG_RULE_START_DATE);
+    }
+
+    private void checkHoldEndDate(JAXBElement<?> jaxbElement, String holdEndDate) {
+        checkDate(jaxbElement, holdEndDate, SedaConstants.TAG_RULE_HOLD_END_DATE);
+    }
+
+    private void checkHoldOwner(JAXBElement<?> jaxbElement, String holdOwner) {
+        assertThat(jaxbElement.getName().getLocalPart()).isEqualTo(SedaConstants.TAG_RULE_HOLD_OWNER);
+        assertThat(jaxbElement.getValue()).isEqualTo(holdOwner);
+    }
+
+    private void checkHoldReassessingDate(JAXBElement<?> jaxbElement, String holdReassessingDate) {
+        checkDate(jaxbElement, holdReassessingDate, SedaConstants.TAG_RULE_HOLD_REASSESSING_DATE);
+    }
+
+    private void checkHoldReason(JAXBElement<?> jaxbElement, String holdReason) {
+        assertThat(jaxbElement.getName().getLocalPart()).isEqualTo(SedaConstants.TAG_RULE_HOLD_REASON);
+        assertThat(jaxbElement.getValue()).isEqualTo(holdReason);
+    }
+
+    private void checkPreventRearrangement(JAXBElement<?> jaxbElement, boolean preventRearrangement) {
+        assertThat(jaxbElement.getName().getLocalPart()).isEqualTo(SedaConstants.TAG_RULE_PREVENT_REARRANGEMENT);
+        assertThat(jaxbElement.getValue()).isInstanceOf(Boolean.class);
+        assertThat(((Boolean) jaxbElement.getValue())).isEqualTo(preventRearrangement);
+    }
+
+    private void checkDate(JAXBElement<?> jaxbElement, String expectedDate, String tagName) {
+        assertThat(jaxbElement.getName().getLocalPart()).isEqualTo(tagName);
+        assertThat(jaxbElement.getValue()).isInstanceOf(XMLGregorianCalendar.class);
+        assertThat(jaxbElement.getValue().toString()).isEqualTo(expectedDate);
     }
 }
