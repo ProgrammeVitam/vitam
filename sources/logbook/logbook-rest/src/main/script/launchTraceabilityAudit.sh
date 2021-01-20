7e5ab1460e435d1f637b784c5d37e528821293ca
Abdelmoez Guetat <abdelmoez.guetat@smile.fr>
2021-01-20 17:39:42 +0000

    Merge branch 'item_7413_reduce_timer_memory_usage' into 'develop'
    Item 7413 : Reduce timer memory usage
    
    See merge request vitam/vitam!8067
    
    (cherry picked from commit b5740d80df82889be1f7c8362bcecef72d58879c)
    
    69650edd Item 7413 : Redure timer memory usage

--- sources/logbook/logbook-rest/src/main/script/launchTraceabilityAudit.sh
+++ sources/logbook/logbook-rest/src/main/script/launchTraceabilityAudit.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-source /vitam/conf/${unix.name}/sysconfig/java_opts
+source /vitam/conf/${unix.name}/sysconfig/timer_java_opts
 
 JAVA_ENTRYPOINT=/vitam/lib/${unix.name}/logbook-administration-audit-${project.version}.jar
 /usr/bin/env java -jar $JAVA_OPTS $JAVA_ENTRYPOINT
