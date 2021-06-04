d6a9cb4bed06a88cc33660536c6b8ce11c11ba0c
Abdellah  Aoudia <abdellah.aoudia.ext@culture.gouv.fr>
2021-06-04 16:04:47 +0200

    Fix 8001 - calcul de l'empreinte d'un fichier absent dans l'offre gestion des erreur lors de la recuperation des fichier dans l'offre

--- vitam-conf-dev/tests/docker-compose.sh
+++ vitam-conf-dev/tests/docker-compose.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-
 MINIO_SSL_CERTIF_FOLDER="/tmp/minio_ssl_certs"
 DOCKER_COMPOSE_DIRNAME="$(dirname ${BASH_SOURCE[0]})"
 VITAMDEV_GIT_REPO="$( cd "$( readlink -f ${DOCKER_COMPOSE_DIRNAME} )/../.." ; pwd )"
-
 echo "Vitam git repo : ${VITAMDEV_GIT_REPO}"
-
 if [ -d "${VITAMDEV_GIT_REPO}/sources/common/common-storage/src/test/resources/s3/tls" ]; then
     echo "Vitam git repo contains S3 certificates folder : ${VITAMDEV_GIT_REPO}/sources/common/common-storage/src/test/resources/s3/tls"
     mkdir -p ${MINIO_SSL_CERTIF_FOLDER}
@@ -16,8 +13,11 @@ else
     echo "Vitam git repo does not contains S3 certificates folder : ${VITAMDEV_GIT_REPO}/sources/common/common-storage/src/test/resources/s3/tls"
     exit 1
 fi
-
-echo "docker-compose --file ${DOCKER_COMPOSE_DIRNAME}/docker-compose.yml up"
-docker-compose --file ${DOCKER_COMPOSE_DIRNAME}/docker-compose.yml up
+echo "docker-compose --file ${DOCKER_COMPOSE_DIRNAME}/docker-compose.yml up -d"
+docker-compose --file ${DOCKER_COMPOSE_DIRNAME}/docker-compose.yml up -d
+docker-compose exec swift "swift/bin/register-swift-endpoint.sh" http://127.0.0.1:8080
+echo "docker-compose --file ${DOCKER_COMPOSE_DIRNAME}/docker-compose.yml up logs -f --tail=all"
+docker-compose --file ${DOCKER_COMPOSE_DIRNAME}/docker-compose.yml logs -f --tail=all
+echo "Stopping docker-compose..."
 echo "docker-compose --file ${DOCKER_COMPOSE_DIRNAME}/docker-compose.yml down"
 docker-compose --file ${DOCKER_COMPOSE_DIRNAME}/docker-compose.yml down
