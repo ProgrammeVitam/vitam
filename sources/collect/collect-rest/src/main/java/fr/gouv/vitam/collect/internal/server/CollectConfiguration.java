d569476fd1a9c1c78c8b0d494b2d2287fc900c7b
Passam KASSEM <passam.kassem@focati-solutions.com>
2021-10-27 15:19:40 +0200

    Add .run to .gitignore file

---
+++ sources/collect/collect-rest/src/main/java/fr/gouv/vitam/collect/internal/server/CollectConfiguration.java
@@ -0,0 +1,6 @@
+package fr.gouv.vitam.collect.internal.server;
+
+import fr.gouv.vitam.common.server.application.configuration.DefaultVitamApplicationConfiguration;
+
+public class CollectConfiguration extends DefaultVitamApplicationConfiguration {
+}
