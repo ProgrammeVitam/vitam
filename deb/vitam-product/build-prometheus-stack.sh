c5d5cebfcc3f70f652227cb294022b743a1111fb
Romain Gordolo <romain.gordolo@teamdlab.com>
2022-03-03 16:44:35 +0100

    Story #9104 Plugin consul exporter for grafana

--- deb/vitam-product/build-prometheus-stack.sh
+++ deb/vitam-product/build-prometheus-stack.sh
@@ -2,14 +2,13 @@
 WORKING_FOLDER=$(dirname $0)
 PROMETHEUS_VERSION="2.19.0"
 PROMETHEUS_NODE_EXPORTER_VERSION="1.0.1"
+PROMETHEUS_CONSUL_EXPORTER_VERSION="0.8.0"
 PROMETHEUS_ALERTMANAGER_VERSION="0.21.0"
-
 
 PROMETHEUS_URL=https://github.com/prometheus/prometheus/releases/download/v${PROMETHEUS_VERSION}/prometheus-${PROMETHEUS_VERSION}.linux-amd64.tar.gz
 PROMETHEUS_NODE_EXPORTER_URL=https://github.com/prometheus/node_exporter/releases/download/v${PROMETHEUS_NODE_EXPORTER_VERSION}/node_exporter-${PROMETHEUS_NODE_EXPORTER_VERSION}.linux-amd64.tar.gz
+PROMETHEUS_CONSUL_EXPORTER_URL=https://github.com/prometheus/consul_exporter/releases/download/v${PROMETHEUS_CONSUL_EXPORTER_VERSION}/consul_exporter-${PROMETHEUS_CONSUL_EXPORTER_VERSION}.linux-amd64.tar.gz
 PROMETHEUS_ALERTMANAGER_URL=https://github.com/prometheus/alertmanager/releases/download/v${PROMETHEUS_ALERTMANAGER_VERSION}/alertmanager-${PROMETHEUS_ALERTMANAGER_VERSION}.linux-amd64.tar.gz
-
-
 
 if [ ! -d ${WORKING_FOLDER}/target ]; then
 	mkdir ${WORKING_FOLDER}/target
@@ -115,6 +114,50 @@ dpkg-deb --build vitam-prometheus-node-exporter ${WORKING_FOLDER}/target
 
 popd
 
+#########################
+## Prometheus consul_exporter
+#########################
+PACKAGE_NAME=consul_exporter
+PACKAGE_VERSION=${PROMETHEUS_CONSUL_EXPORTER_VERSION}
+PACKAGE_URL=${PROMETHEUS_CONSUL_EXPORTER_URL}
+
+rm -rf ${WORKING_FOLDER}/vitam-prometheus-consul-exporter/vitam/app/*
+rm -rf ${WORKING_FOLDER}/vitam-prometheus-consul-exporter/vitam/bin/*
+rm -rf ${WORKING_FOLDER}/vitam-prometheus-consul-exporter/vitam/data/*
+
+mkdir -p ${WORKING_FOLDER}/vitam-prometheus-consul-exporter/vitam/app/${PACKAGE_NAME}
+mkdir -p ${WORKING_FOLDER}/vitam-prometheus-consul-exporter/vitam/bin/${PACKAGE_NAME}
+mkdir -p ${WORKING_FOLDER}/vitam-prometheus-consul-exporter/vitam/data/${PACKAGE_NAME}
+
+
+pushd ${WORKING_FOLDER}/sources/
+echo "Repertoire courant: $(pwd)"
+echo "Récupérer ${PACKAGE_NAME}-${PACKAGE_VERSION}.linux-amd64.tar.gz"
+if [ ! -f ${PACKAGE_NAME}-${PACKAGE_VERSION}.linux-amd64.tar.gz ]; then
+	curl -L -k --max-time 120 ${PACKAGE_URL} --out ${PACKAGE_NAME}-${PACKAGE_VERSION}.linux-amd64.tar.gz
+	if [ ${?} != 0 ]; then
+		echo "Erreur sur le telechargement du fichier tar gz de ${PACKAGE_NAME}: ${PACKAGE_URL}"
+		exit 1
+	fi
+fi
+
+echo "Décompacter ${PACKAGE_NAME}-${PACKAGE_VERSION}.linux-amd64.tar.gz"
+tar xzf ${PACKAGE_NAME}-${PACKAGE_VERSION}.linux-amd64.tar.gz --strip 1 -C ../vitam-prometheus-consul-exporter/vitam/bin/${PACKAGE_NAME}/
+
+popd
+
+pushd ${WORKING_FOLDER}/vitam-prometheus-consul-exporter/vitam/bin/${PACKAGE_NAME}/
+echo "Install files ..."
+
+mv -f LICENSE ../../app/${PACKAGE_NAME}/LICENSE
+mv -f NOTICE ../../app/${PACKAGE_NAME}/NOTICE
+
+popd
+pushd ${WORKING_FOLDER}
+
+dpkg-deb --build vitam-prometheus-consul-exporter ${WORKING_FOLDER}/target
+
+popd
 
 #########################
 ## Prometheus alertmanager
---
+++ rpm/vitam-product/vitam-prometheus-consul-exporter/rpmbuild/SPECS/consul-exporter.spec
@@ -0,0 +1,84 @@
+%define debug_package %{nil}
+
+Name:    vitam-consul-exporter
+Version: 0.8.0
+Release: 1%{?dist}
+Summary: Consul exporter for prometheus.
+License: ASL 2.0
+URL:     https://github.com/prometheus/consul_exporter
+
+Source0: https://github.com/prometheus/consul_exporter/releases/download/v%{version}/consul_exporter-%{version}.linux-amd64.tar.gz
+Source1: %{name}.service
+
+%global vitam_service_name %{name}.service
+%global consul_exporter_datafolder /vitam/data/consul_exporter
+%global consul_exporter_appfolder /vitam/app/consul_exporter
+%global consul_exporter_conffolder /vitam/conf/consul_exporter
+%global consul_exporter_binfolder /vitam/bin/consul_exporter
+%global consul_exporter_logfolder /vitam/log/consul_exporter
+
+%{?systemd_requires}
+Requires(pre): shadow-utils
+Requires:      vitam-user-vitam
+
+%description
+
+Consul exporter 
+This package contains binary to export node metrics to prometheus.
+
+
+%prep
+%setup -q -n consul_exporter-%{version}.linux-amd64
+
+%build
+/bin/true
+
+%install
+mkdir -p %{buildroot}%{consul_exporter_binfolder}
+mkdir -p %{buildroot}%{consul_exporter_appfolder}
+mkdir -p %{buildroot}%{consul_exporter_datafolder}
+mkdir -p %{buildroot}%{consul_exporter_conffolder}
+mkdir -p %{buildroot}%{consul_exporter_conffolder}/sysconfig
+
+install -D -m 755 consul_exporter %{buildroot}%{consul_exporter_binfolder}/consul_exporter
+install -D -m 755 LICENSE %{buildroot}%{consul_exporter_appfolder}/LICENSE
+install -D -m 755 NOTICE %{buildroot}%{consul_exporter_appfolder}/NOTICE
+
+install -D -m 644 %{SOURCE1} %{buildroot}%{_unitdir}/%{vitam_service_name}
+
+%pre
+
+%post
+%systemd_post %{vitam_service_name}
+
+%preun
+%systemd_preun %{vitam_service_name}
+
+%postun
+%systemd_postun %{vitam_service_name}
+
+%clean
+rm -rf %{buildroot}
+
+%files
+%defattr(-,root,root,-)
+
+%{consul_exporter_binfolder}/consul_exporter
+%{consul_exporter_appfolder}/LICENSE
+%{consul_exporter_appfolder}/NOTICE
+%{_unitdir}/%{vitam_service_name}
+
+%dir %attr(750, vitam, vitam) %{consul_exporter_binfolder}
+%dir %attr(750, vitam, vitam) %{consul_exporter_binfolder}
+%dir %attr(750, vitam, vitam) %{consul_exporter_appfolder}
+%dir %attr(750, vitam, vitam) %{consul_exporter_datafolder}
+%dir %attr(750, vitam, vitam) %{consul_exporter_conffolder}
+%dir %attr(750, vitam, vitam) %{consul_exporter_conffolder}/sysconfig
+
+%attr(755, vitam, vitam) %{consul_exporter_binfolder}/consul_exporter
+
+%doc
+
+%changelog
+* Fri Jan 11 2022 French Prime minister Office/SGMAP/DINSIC/Vitam Program <contact.vitam@culture.gouv.fr>
+- Initial version
