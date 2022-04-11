8aca2e7bcd3c3e9694eb2668a18e19bcf7ade65a
Julien Georges <julien.georges@culture.gouv.fr>
2022-04-11 14:48:39 +0200

    Story #9776: Configure Vitam systemd services with ansible.

--- sources/worker/worker-server/src/main/script/worker.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Fork the java service
-java $JAVA_OPTS $JAVA_ENTRYPOINT $JAVA_ARGS &
-# Write the PID for systemd
-echo $! > /vitam/run/${unix.name}/${unix.name}.pid
-exit 0
