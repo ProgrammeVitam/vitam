9468d954c2cfb08554763e83e051fbae0459dafa
Julien Georges <julien.georges@culture.gouv.fr>
2022-08-26 15:20:32 +0000

    Merge branch 'story_10336_fix_scan_avast' into 'develop'
    Story #10336: Properly handle corrupted archive with scan-avast.
    
    See merge request vitam/vitam!9246
    
    (cherry picked from commit 0edb3fc3ea0eba6a2a9e7dc8ab519a8ed188621a)
    
    f07153d6 Story #10336: Properly handle corrupted archive with scan-avast.

--- deployment/environments/antivirus/scan-avast.sh
+++ deployment/environments/antivirus/scan-avast.sh
@@ -31,6 +31,7 @@ custom_scan () {
   scan $SCAN_PARAMS "$FILE_TO_SCAN" &>> ${WORKING_DIR}/scan.log
   local RET=$? # return code of scan
   if [ $RET != $RET_OK ]; then
+    REASON="Virus found !"
     RET=$(($RET+1)) # Increase the return code to fit the expected Vitam's scan code
   fi
 
@@ -41,28 +42,42 @@ custom_scan () {
     # Examples output:
     # avast: /vitam/data/ingest-external/upload/SIP_Test_decompression.zip|>content/ID6999.docx|>word/media/image8.emf: The file is a decompression bomb
     # avast: /vitam/data/ingest-external/aeeaaaaabshmriqsaafowal4jqrjleyaaaaq/aeeaaaaabshmriqsaafowal4jqrjleyaaaaq|>content/ID15.docx|>word/embeddings/Microsoft_PowerPoint_Presentation1.pptx|>ppt/media/image1.emf: The file is a decompression bomb
-    local DECOMPRESSION_BOMBS=($(cat ${WORKING_DIR}/scan.log | grep -e 'The file is a decompression bomb' | rev | cut -d':' -f2 | cut -d'>' -f1 | rev))
+    local DECOMPRESSION_BOMBS=($(cat ${WORKING_DIR}/scan.log | grep -e 'The file is a decompression bomb' | cut -d':' -f2 | cut -d'>' -f2-))
     if [ -n "$DECOMPRESSION_BOMBS" ]; then
-      # Whitelist => emf,wmf
+      # Whitelist => emf,wmf,log
       local REAL_BOMBS=($(echo $DECOMPRESSION_BOMBS | grep -vF -e '.emf' -e '.wmf' -e '.log'))
 
       # If we don't have real decompression bomb (maybe we have too big files)
       if [ -n "$REAL_BOMBS" ]; then
-        echo "${#REAL_BOMBS[@]} real decompression bombs found !" |& tee -a ${WORKING_DIR}/scan.log
+        REASON="$(printf '%s\n' $REAL_BOMBS | wc -l) real decompression bombs found !"
+        echo "ERROR: ${REASON}" |& tee -a ${WORKING_DIR}/scan.log
+        printf ' %s\n' $REAL_BOMBS |& tee -a ${WORKING_DIR}/scan.log
         RET=$RET_VIRUS_FOUND_NOTFIXED
       else
-        echo "${#DECOMPRESSION_BOMBS[@]} decompression bombs ignored..." |& tee -a ${WORKING_DIR}/scan.log
+        echo "INFO: $(printf '%s\n' $DECOMPRESSION_BOMBS | wc -l) decompression bombs ignored..." |& tee -a ${WORKING_DIR}/scan.log
+        printf ' %s\n' $DECOMPRESSION_BOMBS |& tee -a ${WORKING_DIR}/scan.log
         RET=$RET_VIRUS_FOUND_FIXED
       fi
     fi
 
+    # Specific case for corrupted archive
+    # Examples output:
+    # avast: /vitam/data/ingest-external/upload/SIP-Perf-1-18x400M.zip|>content/ID58.tmp: ZIP archive is corrupted
+    # avast: /vitam/data/ingest-external/upload/SIP-Perf-1-18x400M.zip|>content/ID43093.pdf|>word/theme/theme1.xml: ZIP archive is corrupted
+    local CORRUPTED_FILES=($(cat ${WORKING_DIR}/scan.log | grep -e 'ZIP archive is corrupted' | cut -d':' -f2 | tr '|>' '\n' | grep -e content | uniq))
+    if [ -n "$CORRUPTED_FILES" ]; then
+      REASON="$(printf '%s\n' ${CORRUPTED_FILES} | wc -l) corrupted files found !"
+      echo "ERROR: ${REASON}" |& tee -a ${WORKING_DIR}/scan.log
+      printf ' %s\n' $CORRUPTED_FILES |& tee -a ${WORKING_DIR}/scan.log
+      RET=$RET_VIRUS_FOUND_NOTFIXED
+    fi
+
     if [ $RET != $RET_VIRUS_FOUND_NOTFIXED ]; then
-      # Specific case for corrupted or big files
+      # Specific case for big files
       # Example output for zip: avast: /vitam/data/ingest-external/upload/SIP-Perf-1-1x500M.zip|>content/ID13.tmp: Compressed file is too big to be processed
       # Example output for tar.gz: avast: /vitam/data/ingest-external/upload/SIP-Perf-1-1x500M.tar.gz|>SIP-Perf-1-1x500M.tar: Compressed file is too big to be processed
       # Example output for tar.bz2: avast: /vitam/data/ingest-external/upload/SIP-Perf-1-1x500M.tar.bz2|>{bzip}: Compressed file is too big to be processed
-      # Example output for corrupted: avast: /vitam/data/ingest-external/upload/SIP-Perf-1-18x400M.zip|>content/ID58.tmp: ZIP archive is corrupted
-      local FILES_TO_INDIVIDUALLY_SCAN=($(cat ${WORKING_DIR}/scan.log | grep -e 'Compressed file is too big to be processed' -e 'ZIP archive is corrupted' | rev | cut -d':' -f2 | cut -d'>' -f1 | rev))
+      local FILES_TO_INDIVIDUALLY_SCAN=($(cat ${WORKING_DIR}/scan.log | grep -e 'Compressed file is too big to be processed'| cut -d':' -f2 | cut -d'>' -f2 | uniq | sed 's/|//'))
       if [ -n "$FILES_TO_INDIVIDUALLY_SCAN" ]; then
         # echo "Starting individual scan for ${#FILES_TO_INDIVIDUALLY_SCAN[@]} big files..." |& tee -a ${WORKING_DIR}/scan.log
         unzip_and_scan "$FILE_TO_SCAN" "${FILES_TO_INDIVIDUALLY_SCAN[@]}"
@@ -89,6 +104,7 @@ unzip_and_scan () {
 
     if [ "$TYPE_SIP" == 'application/zip' ]; then
       unzip $UNZIP_PARAMS "$ARCHIVE" "${FILE}" -d "$WORKING_DIR" |& tee -a ${WORKING_DIR}/scan.log
+      # $? == 11 file not found; skipping to avoid infinite loop
     elif [ "$TYPE_SIP" == 'application/x-tar' ]; then
       tar xvf "$ARCHIVE" --directory "$WORKING_DIR" "${FILE}" |& tee -a ${WORKING_DIR}/scan.log
     elif [ "$TYPE_SIP" == 'application/gzip' ] || [ "$TYPE_SIP" == 'application/x-gzip' ]; then
@@ -121,12 +137,12 @@ unzip_and_scan () {
 ################################################################################
 
 if [ $# -ne 1 ]; then # Argument number must be one
-  echo "ERROR : $# parameter(s) provided, only one parameter is needed"
+  echo "ERROR: $# parameter(s) provided, only one parameter is needed"
 
 else # one argument, let's go
   SIP=$1
   if [ ! -f "$SIP" ];then # if the file wich will be scan is existing, keep going
-    echo "ERROR : \"$SIP\" doesn't exit"
+    echo "ERROR: \"$SIP\" doesn't exit"
 
   else
     START_TIME=$(date +%s)
@@ -154,14 +170,12 @@ else # one argument, let's go
     if [ $RET == $RET_OK ]; then
       RET_MSG='[OK]'
     elif [ $RET == $RET_VIRUS_FOUND_FIXED ]; then
-      RET_MSG='[OK: Virus found but ignored]'
+      RET_MSG='[OK: Virus found but ignored.]'
       RET=$RET_OK # FORCE O UNTIL RET=1 WELL HANDLED BY VITAM
     elif [ $RET == $RET_VIRUS_FOUND_NOTFIXED ]; then
-      REASON=$(cat ${WORKING_DIR}/scan.log | tail --lines=1 | cut -f2-)
-      RET_MSG="[KO: Virus found - $REASON]"
+      RET_MSG="[KO: $REASON]"
     elif [ $RET == $RET_FAILURE ]; then
-      REASON=$(cat ${WORKING_DIR}/scan.log | tail --lines=1 | cut -d":" -f3- | xargs)
-      RET_MSG="[ERROR: Scan not performed - $REASON]"
+      RET_MSG="[ERROR: Scan not performed.]"
     fi
 
     END_TIME=$(date +%s)
