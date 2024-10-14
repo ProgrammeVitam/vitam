0fe6fde63098e1bb04b40bbcf5b65072d8de9d7f
Lotfi GHERIBI <lotfi.gheribi.ext@culture.gouv.fr>
2024-10-14 16:00:24 +0200

    Bug #13562: Disable rectification mode of metadata consistency audit

--- sources/metadata/metadata-core/src/main/java/fr/gouv/vitam/metadata/core/config/MetaDataConfiguration.java
+++ sources/metadata/metadata-core/src/main/java/fr/gouv/vitam/metadata/core/config/MetaDataConfiguration.java
@@ -78,7 +78,10 @@ public class MetaDataConfiguration extends DbConfigurationImpl {
     private MetadataIndexationConfiguration indexationConfiguration;
 
     @JsonProperty("isDataConsistencyAuditRunnable")
-    private Boolean isDataConsistencyAuditRunnable;
+    private boolean isDataConsistencyAuditRunnable;
+
+    @JsonProperty("enableDataConsistencyRectificationMode")
+    private boolean enableDataConsistencyRectificationMode;
 
     @JsonProperty("dataConsistencyAuditOplogMaxSize")
     private Integer dataConsistencyAuditOplogMaxSize;
@@ -329,12 +332,23 @@ public class MetaDataConfiguration extends DbConfigurationImpl {
         return this;
     }
 
-    public Boolean getIsDataConsistencyAuditRunnable() {
+    public boolean getIsDataConsistencyAuditRunnable() {
         return isDataConsistencyAuditRunnable;
     }
 
-    public void setIsDataConsistencyAuditRunnable(Boolean dataConsistencyAuditRunnable) {
+    public void setIsDataConsistencyAuditRunnable(boolean dataConsistencyAuditRunnable) {
         isDataConsistencyAuditRunnable = dataConsistencyAuditRunnable;
+    }
+
+    public boolean isEnableDataConsistencyRectificationMode() {
+        return enableDataConsistencyRectificationMode;
+    }
+
+    public MetaDataConfiguration setEnableDataConsistencyRectificationMode(
+        boolean enableDataConsistencyRectificationMode
+    ) {
+        this.enableDataConsistencyRectificationMode = enableDataConsistencyRectificationMode;
+        return this;
     }
 
     public Integer getDataConsistencyAuditOplogMaxSize() {
