e81c8809e1cbea23f7c0a048864c3850d1bf2871
Maxime Robert <maxime.robert@smile.fr>
2025-01-21 14:08:28 +0100

    Fix #14193: SigningInformation.* & PersistentIdentifier.PersistentIdentifierContent SEDA versions

--- sources/integration-test/src/test/java/fr/gouv/vitam/access/external/AccessExternalIT.java
+++ sources/integration-test/src/test/java/fr/gouv/vitam/access/external/AccessExternalIT.java
@@ -794,10 +794,7 @@ public class AccessExternalIT extends VitamRuleRunner {
         assertEquals("PersistentIdentifierContent", persistentIdentifierContentElt.getSedaField());
         assertEquals(MetadataType.UNIT.getName(), persistentIdentifierContentElt.getCollection());
         assertEquals(SchemaCardinality.ONE_REQUIRED, persistentIdentifierContentElt.getCardinality());
-        assertThat(persistentIdentifierContentElt.getSedaVersions()).containsExactlyInAnyOrder(
-            // "2.2", // FIXME: should also contain 2.2?
-            "2.3"
-        );
+        assertThat(persistentIdentifierContentElt.getSedaVersions()).containsExactlyInAnyOrder("2.1", "2.2", "2.3");
     }
 
     @RunWithCustomExecutor
@@ -916,7 +913,7 @@ public class AccessExternalIT extends VitamRuleRunner {
         assertEquals("PersistentIdentifierContent", persistentIdentifierContentElt.getSedaField());
         assertEquals(MetadataType.OBJECTGROUP.getName(), persistentIdentifierContentElt.getCollection());
         assertEquals(SchemaCardinality.ONE, persistentIdentifierContentElt.getCardinality());
-        assertThat(persistentIdentifierContentElt.getSedaVersions()).containsExactlyInAnyOrder("2.2", "2.3");
+        assertThat(persistentIdentifierContentElt.getSedaVersions()).containsExactlyInAnyOrder("2.3");
     }
 
     @Test
@@ -946,8 +943,8 @@ public class AccessExternalIT extends VitamRuleRunner {
             );
 
             JsonAssert.assertJsonEquals(
-                JsonHandler.toJsonNode(archiveUnitProfileSchema).get("$results"),
                 expectedArchiveUnitSchema,
+                JsonHandler.toJsonNode(archiveUnitProfileSchema).get("$results"),
                 JsonAssert.when(Option.IGNORING_ARRAY_ORDER)
             );
         } catch (VitamClientException e) {
