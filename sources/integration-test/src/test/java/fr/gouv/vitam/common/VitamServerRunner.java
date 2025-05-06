5de82aa3ccaf75a5ced6570a10da589110c7da40
Lotfi GHERIBI <lotfi.gheribi.ext@culture.gouv.fr>
2025-05-06 18:16:05 +0200

    Bug #14681: Fix process state after pause request

--- sources/integration-test/src/test/java/fr/gouv/vitam/common/VitamServerRunner.java
+++ sources/integration-test/src/test/java/fr/gouv/vitam/common/VitamServerRunner.java
@@ -885,6 +885,8 @@ public class VitamServerRunner extends ExternalResource {
         logbookMain.start();
         SystemPropertyUtil.clear(LogbookMain.PARAMETER_JETTY_SERVER_PORT);
         // Wait startup
+        waitServerStart(LogbookOperationsClientFactory.getInstance().getClient());
+        waitServerStart(LogbookLifeCyclesClientFactory.getInstance().getClient());
     }
 
     public void stopLogbookServer(boolean mockWhenStop) throws VitamApplicationServerException {
