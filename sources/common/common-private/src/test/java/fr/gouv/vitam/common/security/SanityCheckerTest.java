5cb374bff561deb497f16353d53ff63d3c95e7a9
Lotfi GHERIBI <lotfi.gheribi.ext@culture.gouv.fr>
2025-07-24 17:17:43 +0200

    Bug #14988: Enhanced request validation

--- sources/common/common-private/src/test/java/fr/gouv/vitam/common/security/SanityCheckerTest.java
+++ sources/common/common-private/src/test/java/fr/gouv/vitam/common/security/SanityCheckerTest.java
@@ -27,9 +27,13 @@
 package fr.gouv.vitam.common.security;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.node.TextNode;
 import fr.gouv.vitam.common.PropertiesUtils;
 import fr.gouv.vitam.common.StringUtils;
 import fr.gouv.vitam.common.exception.InvalidParseOperationException;
+import fr.gouv.vitam.common.guid.GUIDFactory;
 import fr.gouv.vitam.common.json.JsonHandler;
 import jakarta.ws.rs.core.Cookie;
 import jakarta.ws.rs.core.HttpHeaders;
@@ -127,6 +131,17 @@ public class SanityCheckerTest {
         for (final String pathFile : pathsXMLowaspKO) {
             filesOwaspKO.add(PropertiesUtils.findFile(pathFile));
         }
+    }
+
+    @Test(timeout = 30_000)
+    public void sanitizeLargeArrays() throws Exception {
+        ArrayNode arrayNode = JsonHandler.createArrayNode();
+        TextNode stringValue = new TextNode(GUIDFactory.newGUID().toString());
+        for (int i = 0; i < 10_000; i++) {
+            arrayNode.add(stringValue);
+        }
+        ObjectNode json = JsonHandler.createObjectNode().set("someArray", arrayNode);
+        SanityChecker.checkJsonAll(json);
     }
 
     @Test(expected = InvalidParseOperationException.class)
