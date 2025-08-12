d469449a6cc531147a48c42ca754c84a0c8481ab
mohamed tizaoui <mohamed.tizaoui@teamdlab.com>
2025-08-12 14:09:11 +0200

    BUG #15003: fix check deletion status

--- sources/collect/collect-external/collect-external-rest/src/main/java/fr/gouv/vitam/collect/external/external/rest/TransactionExternalResource.java
+++ sources/collect/collect-external/collect-external-rest/src/main/java/fr/gouv/vitam/collect/external/external/rest/TransactionExternalResource.java
@@ -549,7 +549,7 @@ public class TransactionExternalResource extends ApplicationStatusResource {
             return Response.status(PRECONDITION_FAILED).build();
         } catch (VitamClientException e) {
             LOGGER.error("Error when launching reclassification", e);
-            return Response.status(BAD_REQUEST).build();
+            return CollectRequestResponse.toVitamError(BAD_REQUEST, e.getLocalizedMessage());
         }
     }
 
@@ -583,7 +583,7 @@ public class TransactionExternalResource extends ApplicationStatusResource {
             return Response.status(PRECONDITION_FAILED).build();
         } catch (Exception e) {
             LOGGER.error("Error when launching deletion", e);
-            return Response.status(BAD_REQUEST).build();
+            return CollectRequestResponse.toVitamError(BAD_REQUEST, e.getLocalizedMessage());
         }
     }
 
