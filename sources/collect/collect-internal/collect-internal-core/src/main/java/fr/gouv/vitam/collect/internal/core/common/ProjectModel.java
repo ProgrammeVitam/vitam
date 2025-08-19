34e2d2c7be0581fc46eaff1a22ef93573a02a0d6
Lotfi GHERIBI <lotfi.gheribi.ext@culture.gouv.fr>
2025-08-19 14:55:16 +0200

    Bug #15057: Fix Collect project field name mismatch

--- sources/collect/collect-internal/collect-internal-core/src/main/java/fr/gouv/vitam/collect/internal/core/common/ProjectModel.java
+++ sources/collect/collect-internal/collect-internal-core/src/main/java/fr/gouv/vitam/collect/internal/core/common/ProjectModel.java
@@ -74,7 +74,7 @@ public class ProjectModel {
     @JsonProperty("ArchivingSystemTenant")
     private Integer archivingSystemTenant;
 
-    @JsonProperty("ConnectedToExternalSystem")
+    @JsonProperty("ConnectedToArchivingSystem")
     private Boolean connectedToArchivingSystem;
 
     /**
