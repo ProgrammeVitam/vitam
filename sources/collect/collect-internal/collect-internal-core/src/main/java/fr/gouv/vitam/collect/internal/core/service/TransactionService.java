27dea627710a7d1799535ef1ff7f26b0b5cd1c43
Julien Georges <julien.georges@culture.gouv.fr>
2026-05-12 17:54:42 +0200

    Merge branch 'bug_16427' into 'develop'
    Bug #16427: Do not abort the entire batch update process on first error and check LogbookOperationId before updating result.
    
    See merge request vitam/vitam!11095
    
    (cherry picked from commit 8f2bf9dfb2ce20d589856728931d383be5623f17)
    
    ad87d56a Bug #16427: Do not abort the entire batch update process on first error and...
    
    Co-authored-by: Julien Georges <julien.georges@culture.gouv.fr>

--- sources/collect/collect-internal/collect-internal-core/src/main/java/fr/gouv/vitam/collect/internal/core/service/TransactionService.java
+++ sources/collect/collect-internal/collect-internal-core/src/main/java/fr/gouv/vitam/collect/internal/core/service/TransactionService.java
@@ -401,12 +401,16 @@ public class TransactionService {
                 }
                 RequestResponseOK<JsonNode> requestResponseOK = (RequestResponseOK<JsonNode>) requestResponse;
                 List<LogbookOperation> logbookOperations = getFromJsonNodeList(
-                    (requestResponseOK).getResults(),
+                    requestResponseOK.getResults(),
                     new TypeReference<>() {}
                 );
-                logbookOperations.forEach(
-                    logbookOperation -> results.put(logbookOperation.getId(), getOperationStatus(logbookOperation))
-                );
+                logbookOperations.forEach(logbookOperation -> {
+                    if (logbookOperation.getId() != null) {
+                        results.put(logbookOperation.getId(), getOperationStatus(logbookOperation));
+                    } else {
+                        LOGGER.error("Invalid state. LogbookOperation id is null " + logbookOperation);
+                    }
+                });
             }
 
             List<String> notFoundTransactionIds = transactionIds
@@ -416,9 +420,6 @@ public class TransactionService {
 
             if (CollectionUtils.isNotEmpty(notFoundTransactionIds)) {
                 LOGGER.error("Invalid state. Transactions ids have not been found " + notFoundTransactionIds);
-                throw new CollectInternalException(
-                    "Invalid state. At least one transaction have not been found in Vitam"
-                );
             }
 
             return results;
@@ -426,6 +427,7 @@ public class TransactionService {
             LOGGER.error("Error when select operation:", e);
             throw new CollectInternalException(e);
         } catch (LogbookClientException | InvalidParseOperationException e) {
+            LOGGER.error("Error parsing or fetching logbook operations:", e);
             throw new CollectInternalException(e);
         }
     }
